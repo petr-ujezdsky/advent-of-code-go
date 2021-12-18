30627790591098e9634385d5f8ef31c5c013d88e
petr-ujezdsky <petr.ujezdsky@gmail.com>
2021-12-18 01:36:04 +0100

    Day 02 / 02

--- 2021/day_02/main_test.go
+++ 2021/day_02/main_test.go
@@ -26,6 +26,48 @@ func Test_01_example(t *testing.T) {
 	assert.Equal(t, 150, result)
 }
 
+func Test_01(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	commands, err := ParseToCommands(reader)
+	assert.Nil(t, err)
+
+	x, y, result := move(commands)
+
+	assert.Equal(t, 1968, x)
+	assert.Equal(t, 1063, y)
+	assert.Equal(t, 2091984, result)
+}
+
+func Test_02_example(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	commands, err := ParseToCommands(reader)
+	assert.Nil(t, err)
+
+	x, y, result := moveByAim(commands)
+
+	assert.Equal(t, 15, x)
+	assert.Equal(t, 60, y)
+	assert.Equal(t, 900, result)
+}
+
+func Test_02(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	commands, err := ParseToCommands(reader)
+	assert.Nil(t, err)
+
+	x, y, result := moveByAim(commands)
+
+	assert.Equal(t, 1968, x)
+	assert.Equal(t, 1060092, y)
+	assert.Equal(t, 2086261056, result)
+}
+
 type Command struct {
 	MoveX, MoveY int
 }
@@ -72,6 +114,23 @@ func move(commands []Command) (int, int, int) {
 	for _, command := range commands {
 		x += command.MoveX
 		y += command.MoveY
+	}
+
+	return x, y, x * y
+}
+
+func moveByAim(commands []Command) (int, int, int) {
+	x := 0
+	y := 0
+	aim := 0
+
+	for _, command := range commands {
+		x += command.MoveX
+		aim += command.MoveY
+
+		if command.MoveX != 0 {
+			y += aim * command.MoveX
+		}
 	}
 
 	return x, y, x * y
