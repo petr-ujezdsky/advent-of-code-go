fccb0a2f74d96e97bc011a2f3927611ac6cd8adc
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-11-19 21:49:14 +0100

    Generic Vector2

---
+++ utils/vector2.go
@@ -0,0 +1,21 @@
+package utils
+
+type vector2[T any] struct {
+	X, Y T
+}
+
+type Number interface {
+	int | float64
+}
+
+type vector2n[T Number] vector2[T]
+
+type Vector2i = vector2n[int]
+type Vector2f = vector2n[float64]
+
+func (v1 vector2n[T]) Add(v2 vector2n[T]) vector2n[T] {
+	return vector2n[T]{
+		X: v1.X + v2.X,
+		Y: v1.Y + v2.Y,
+	}
+}
