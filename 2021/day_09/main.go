2772835867049542481c89d59fe59c7ff725835e
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-11-19 17:37:46 +0100

    Day 09 / 01

--- 2021/day_09/main.go
+++ 2021/day_09/main.go
@@ -8,6 +8,57 @@ import (
 
 type HeightMap = utils.Matrix2[int]
 
+func ValueAt(x, y int, heightMap HeightMap, width, height int) (int, bool) {
+	if x < 0 || x >= width || y < 0 || y >= height {
+		return 0, false
+	}
+
+	return heightMap[x][y], true
+}
+
+func InspectNeighbours(heightMap HeightMap, x, y, width, height int, offsetIndexes []int) (int, bool) {
+	value := heightMap[x][y]
+
+	for _, dx := range offsetIndexes {
+		neighbour, ok := ValueAt(x+dx, y, heightMap, width, height)
+		if ok && neighbour <= value {
+			// found neighbour of lower value
+			return 0, false
+		}
+	}
+
+	for _, dy := range offsetIndexes {
+		neighbour, ok := ValueAt(x, y+dy, heightMap, width, height)
+		if ok && neighbour <= value {
+			// found neighbour of lower value
+			return 0, false
+		}
+	}
+
+	riskLevel := value + 1
+	return riskLevel, true
+}
+
+func FindLowPointsAndSum(heightMap HeightMap) int {
+	width := len(heightMap)
+	height := len(heightMap[0])
+
+	offsetIndexes := []int{-1, 1}
+
+	lowPointsRiskLevelsSum := 0
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			riskLevel, ok := InspectNeighbours(heightMap, x, y, width, height, offsetIndexes)
+			if ok {
+				lowPointsRiskLevelsSum += riskLevel
+			}
+		}
+	}
+
+	return lowPointsRiskLevelsSum
+}
+
 func ParseInput(r io.Reader) (HeightMap, error) {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanLines)
--- 2021/day_09/main_test.go
+++ 2021/day_09/main_test.go
@@ -20,3 +20,25 @@ func Test_01_example_parse(t *testing.T) {
 	// last column
 	assert.Equal(t, []int{0, 1, 2, 9, 8}, heightMap[9])
 }
+
+func Test_01_example(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	heightMap, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	sum := FindLowPointsAndSum(heightMap)
+	assert.Equal(t, 15, sum)
+}
+
+func Test_01(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	heightMap, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	sum := FindLowPointsAndSum(heightMap)
+	assert.Equal(t, 486, sum)
+}
