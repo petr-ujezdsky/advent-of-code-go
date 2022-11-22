88447e515e40469299d6abc7511504a9ce8bd65e
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-11-23 00:34:41 +0100

    Day 14 / 01

--- 2021/day_14/main.go
+++ 2021/day_14/main.go
@@ -1,14 +1,68 @@
-package day_12
+package day_14
 
 import (
 	"bufio"
 	"io"
+	"math"
 	"strings"
 )
 
 type World struct {
 	template string
 	rules    map[string]string
+}
+
+func PolymerScore(polymer string) int {
+	counts := make(map[rune]int)
+
+	for _, char := range []rune(polymer) {
+		counts[char]++
+	}
+
+	minCount := math.MaxInt
+	maxCount := math.MinInt
+
+	for _, count := range counts {
+		if count > maxCount {
+			maxCount = count
+		}
+
+		if count < minCount {
+			minCount = count
+		}
+	}
+
+	return maxCount - minCount
+}
+
+func GrowPolymerStep(template string, rules map[string]string) string {
+	var polymer strings.Builder
+
+	for i := 0; i < len(template)-1; i++ {
+		duo := template[i : i+2]
+
+		polymer.WriteString(string(duo[0]))
+
+		newChar, contains := rules[duo]
+		if contains {
+			polymer.WriteString(newChar)
+		} else {
+			polymer.WriteString(string(duo[1]))
+		}
+	}
+
+	// last char
+	polymer.WriteString(string(template[len(template)-1]))
+
+	return polymer.String()
+}
+
+func GrowPolymer(template string, rules map[string]string, stepsCount int) string {
+	for i := 0; i < stepsCount; i++ {
+		template = GrowPolymerStep(template, rules)
+	}
+
+	return template
 }
 
 func ParseInput(r io.Reader) (World, error) {
--- 2021/day_14/main_test.go
+++ 2021/day_14/main_test.go
@@ -1,4 +1,4 @@
-package day_12
+package day_14
 
 import (
 	"os"
@@ -18,4 +18,60 @@ func Test_01_example_parse(t *testing.T) {
 	assert.Equal(t, 16, len(world.rules))
 	assert.Equal(t, "B", world.rules["CH"])
 	assert.Equal(t, "C", world.rules["CN"])
+}
+
+func Test_01_example_single(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	polymer := world.template
+	polymer = GrowPolymerStep(polymer, world.rules)
+	assert.Equal(t, "NCNBCHB", polymer)
+
+	polymer = GrowPolymerStep(polymer, world.rules)
+	assert.Equal(t, "NBCCNBBBCBHCB", polymer)
+
+	polymer = GrowPolymerStep(polymer, world.rules)
+	assert.Equal(t, "NBBBCNCCNBBNBNBBCHBHHBCHB", polymer)
+
+	polymer = GrowPolymerStep(polymer, world.rules)
+	assert.Equal(t, "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", polymer)
+}
+
+func Test_01_example_multi(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	polymer := GrowPolymer(world.template, world.rules, 4)
+	assert.Equal(t, "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", polymer)
+}
+
+func Test_01_example(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	polymer := GrowPolymer(world.template, world.rules, 10)
+	score := PolymerScore(polymer)
+	assert.Equal(t, 1588, score)
+}
+
+func Test_01(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	world, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	polymer := GrowPolymer(world.template, world.rules, 10)
+	score := PolymerScore(polymer)
+	assert.Equal(t, 3555, score)
 }
