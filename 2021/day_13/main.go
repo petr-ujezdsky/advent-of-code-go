cd0546493c249f5a21fa5b27cefdde6d7148ae7e
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-11-22 23:08:18 +0100

    Day 13 / 01

--- 2021/day_13/main.go
+++ 2021/day_13/main.go
@@ -2,6 +2,7 @@ package day_12
 
 import (
 	"bufio"
+	"fmt"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"io"
 	"strconv"
@@ -9,20 +10,82 @@ import (
 )
 
 type Fold struct {
-	index int
-	dirX  bool
+	index      int
+	horizontal bool
 }
 
 type World struct {
-	points []utils.Vector2i
+	points []*utils.Vector2i
 	folds  []Fold
+}
+
+func foldPoint(point *utils.Vector2i, fold Fold) utils.Vector2i {
+	if fold.horizontal && point.Y > fold.index {
+		// fold up
+		y := fold.index - (point.Y - fold.index)
+		return utils.Vector2i{point.X, y}
+	} else if !fold.horizontal && point.X > fold.index {
+		// fold left
+		x := fold.index - (point.X - fold.index)
+		return utils.Vector2i{x, point.Y}
+	}
+
+	// no change
+	return *point
+}
+
+func foldPoints(points []*utils.Vector2i, fold Fold) {
+	for _, point := range points {
+		*point = foldPoint(point, fold)
+	}
+}
+
+func countUniquePoints(points []*utils.Vector2i) int {
+	uniquePoints := make(map[int]int)
+
+	for _, point := range points {
+		hash := point.X*1_000_000 + point.Y
+		uniquePoints[hash]++
+	}
+
+	return len(uniquePoints)
+}
+
+func printPoints(points []*utils.Vector2i) {
+	var maxX, maxY int
+
+	for _, point := range points {
+		maxX = utils.Max(maxX, point.X)
+		maxY = utils.Max(maxY, point.Y)
+	}
+
+	matrix := utils.NewMatrix2[int](maxX+1, maxY+1)
+
+	for _, point := range points {
+		matrix.Columns[point.X][point.Y]++
+	}
+
+	fmt.Println(matrix.StringFmt(utils.FmtBoolean[int]))
+}
+
+func FoldPaper(world World, foldsCount int) int {
+	for i, fold := range world.folds {
+		if i >= foldsCount {
+			break
+		}
+		foldPoints(world.points, fold)
+	}
+
+	//printPoints(world.points)
+
+	return countUniquePoints(world.points)
 }
 
 func ParseInput(r io.Reader) (World, error) {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanLines)
 
-	var points []utils.Vector2i
+	var points []*utils.Vector2i
 
 	var maxX, maxY int
 
@@ -30,15 +93,22 @@ func ParseInput(r io.Reader) (World, error) {
 	for scanner.Scan() && scanner.Text() != "" {
 		coordinate := strings.Split(scanner.Text(), ",")
 
-		pos := utils.Vector2i{
-			X: int(coordinate[0][0] - '0'),
-			Y: int(coordinate[1][0] - '0'),
+		x, err := strconv.Atoi(coordinate[0])
+		if err != nil {
+			return World{}, err
 		}
+
+		y, err := strconv.Atoi(coordinate[1])
+		if err != nil {
+			return World{}, err
+		}
+
+		pos := utils.Vector2i{x, y}
 
 		maxX = utils.Max(maxX, pos.X)
 		maxY = utils.Max(maxY, pos.Y)
 
-		points = append(points, pos)
+		points = append(points, &pos)
 	}
 
 	// folds
@@ -53,8 +123,8 @@ func ParseInput(r io.Reader) (World, error) {
 		}
 
 		fold := Fold{
-			index: index,
-			dirX:  foldParts[0][len(foldParts[0])-1] == 'x',
+			index:      index,
+			horizontal: foldParts[0][len(foldParts[0])-1] == 'y',
 		}
 
 		folds = append(folds, fold)
--- 2021/day_13/main_test.go
+++ 2021/day_13/main_test.go
@@ -1,6 +1,7 @@
 package day_12
 
 import (
+	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"os"
 	"testing"
 
@@ -15,10 +16,32 @@ func Test_01_example_parse(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, 18, len(world.points))
-	assert.Equal(t, 6, world.points[0].X)
-	assert.Equal(t, 10, world.points[0].Y)
+	assert.Equal(t, utils.Vector2i{6, 10}, *world.points[0])
+	assert.Equal(t, utils.Vector2i{9, 0}, *world.points[17])
 
 	assert.Equal(t, 2, len(world.folds))
-	assert.Equal(t, 7, world.folds[0].index)
-	assert.Equal(t, false, world.folds[0].dirX)
+	assert.Equal(t, Fold{7, true}, world.folds[0])
+	assert.Equal(t, Fold{5, false}, world.folds[1])
+}
+
+func Test_01_example(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	dotsCount := FoldPaper(world, 1)
+	assert.Equal(t, 17, dotsCount)
+}
+
+func Test_01(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	world, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	dotsCount := FoldPaper(world, 1)
+	assert.Equal(t, 655, dotsCount)
 }
