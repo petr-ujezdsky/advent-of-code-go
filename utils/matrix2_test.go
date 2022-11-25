9df73d1ccc8bc2b3ddf8064365def59f8608e533
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-11-25 12:38:42 +0100

    Matrix2 data locality test

---
+++ utils/matrix2_test.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewMatrix2_data_locality(t *testing.T) {
+	matrix2 := NewMatrix2[int](2, 3)
+
+	i := 0
+	for x := 0; x < 2; x++ {
+		for y := 0; y < 3; y++ {
+			matrix2.Columns[x][y] = i
+			i++
+		}
+	}
+
+	// ensured data locality
+	assert.Equal(t, "[[0 1 2] [3 4 5]]", fmt.Sprint(matrix2.Columns))
+}
