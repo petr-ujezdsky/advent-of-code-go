419fa984528316045219136f4f815e4c0084f686
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-11-29 22:18:45 +0100

    Day 19 / 01 functional but slow

--- 2021/day_19/main.go
+++ 2021/day_19/main.go
@@ -2,6 +2,7 @@ package day_19
 
 import (
 	"bufio"
+	"fmt"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"io"
 	"regexp"
@@ -16,6 +17,7 @@ var regexHeader = regexp.MustCompile("--- scanner (\\d+) ---")
 type BeaconScanner struct {
 	Id             int
 	RotatedBeacons [][]Vector3i
+	UniqueBeacons  map[Vector3i]struct{}
 }
 
 func roll(positions []Vector3i) []Vector3i {
@@ -37,27 +39,127 @@ func turn(positions []Vector3i) []Vector3i {
 // allRotations returns all 24 variants of rotated vectors. The original vectors are in first element
 // see https://stackoverflow.com/a/16467849/1310733
 func allRotations(positions []Vector3i) [][]Vector3i {
-	length := 24
-	rotations := make([][]Vector3i, length)
+	rotations := make([][]Vector3i, 24)
 	i := 0
 	for cycle := 0; cycle < 2; cycle++ {
 		// 3x RTTT
 		for step := 0; step < 3; step++ {
 			// R
 			positions = roll(positions)
-			rotations[length-i-1] = positions
+			rotations[i] = positions
 			i++
 
 			// 3x T
 			for t := 0; t < 3; t++ {
 				positions = turn(positions)
-				rotations[length-i-1] = positions
+				rotations[i] = positions
 				i++
+			}
+		}
+
+		// RTR for next cycle
+		positions = roll(turn(roll(positions)))
+	}
+
+	// original positions as first "rotation"
+	rotations[0], rotations[11] = rotations[11], rotations[0]
+
+	return rotations
+}
+
+func FindOverlap(s1, s2 []Vector3i) (bool, Vector3i) {
+	// choose b1
+	for _, b1 := range s1 {
+		// choose b2
+		for _, b2 := range s2 {
+			count := 0
+
+			// try to align all s2 beacons using vec b2 -> b1
+			step := b1.Subtract(b2)
+
+			// find same beacons
+			for _, bb1 := range s1 {
+				for _, bb2 := range s2 {
+					// align using step
+					bb2 = bb2.Add(step)
+
+					if bb1 == bb2 {
+						count++
+
+						if count >= 12 {
+							return true, step
+						}
+
+						//break
+					}
+				}
 			}
 		}
 	}
 
-	return rotations
+	return false, Vector3i{}
+}
+
+func FindOverlapRotations(s1, s2 BeaconScanner) (bool, int, Vector3i) {
+	// over all s2 rotations
+	for irot, s2beacons := range s2.RotatedBeacons {
+		overlap, step := FindOverlap(s1.RotatedBeacons[0], s2beacons)
+		if overlap {
+			return true, irot, step
+		}
+	}
+
+	return false, 0, Vector3i{}
+}
+
+func consume(mainScanner BeaconScanner, beacons []Vector3i, step Vector3i) BeaconScanner {
+	var newBeacons []Vector3i
+
+	for _, beacon := range beacons {
+		beacon = beacon.Add(step)
+
+		// beacon is new
+		if _, ok := mainScanner.UniqueBeacons[beacon]; !ok {
+			newBeacons = append(newBeacons, beacon)
+			// add to unique set
+			mainScanner.UniqueBeacons[beacon] = struct{}{}
+		}
+	}
+
+	// calculate all rotations
+	rotations := allRotations(newBeacons)
+
+	// merge with mainScanner
+	for i := range mainScanner.RotatedBeacons {
+		mainScanner.RotatedBeacons[i] = append(mainScanner.RotatedBeacons[i], rotations[i]...)
+	}
+
+	return mainScanner
+}
+
+func SearchAndConsume(scanners []BeaconScanner) BeaconScanner {
+	mainScanner := scanners[0]
+	scanners = utils.RemoveUnordered(scanners, 0)
+
+	for len(scanners) > 0 {
+		for i, scanner := range scanners {
+			fmt.Printf("Finding overlap in %d / %d\n", i, len(scanners))
+
+			ok, irot, step := FindOverlapRotations(mainScanner, scanner)
+			if ok {
+				// consume scanner
+				mainScanner = consume(mainScanner, scanner.RotatedBeacons[irot], step)
+
+				// remove scanner
+				scanners = utils.RemoveUnordered(scanners, i)
+
+				fmt.Printf("Found! Consuming %d\n", i)
+				break
+			}
+		}
+	}
+
+	return mainScanner
 }
 
 func ParseInput(r io.Reader) ([]BeaconScanner, error) {
@@ -95,7 +197,16 @@ func ParseInput(r io.Reader) ([]BeaconScanner, error) {
 			beacons = append(beacons, Vector3i{x, y, z})
 		}
 
-		beaconScanners = append(beaconScanners, BeaconScanner{id, allRotations(beacons)})
+		// find all rotations
+		rotations := allRotations(beacons)
+
+		// add unique beacons
+		uniqueBeacons := make(map[Vector3i]struct{})
+		for _, beacon := range rotations[0] {
+			uniqueBeacons[beacon] = struct{}{}
+		}
+
+		beaconScanners = append(beaconScanners, BeaconScanner{id, rotations, uniqueBeacons})
 	}
 
 	return beaconScanners, nil
--- 2021/day_19/main_test.go
+++ 2021/day_19/main_test.go
@@ -1,6 +1,7 @@
 package day_19
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
@@ -9,6 +10,17 @@ import (
 func Test_regexHeader(t *testing.T) {
 	matches := regexHeader.FindStringSubmatch("--- scanner 5 ---")
 	assert.Equal(t, []string{"--- scanner 5 ---", "5"}, matches)
+}
+
+func Test_printAllRotations(t *testing.T) {
+	rots := allRotations([]Vector3i{{686, 422, 578}})
+	for i, rot := range rots {
+		if rot[0] == (Vector3i{686, 422, 578}) {
+			fmt.Printf("* %0d %s\n", i, rot)
+		} else {
+			fmt.Printf("  %0d %s\n", i, rot)
+		}
+	}
 }
 
 func Test_01_example_parse(t *testing.T) {
@@ -25,4 +37,28 @@ func Test_01_example_parse(t *testing.T) {
 	assert.Equal(t, 4, scanners[4].Id)
 	assert.Equal(t, Vector3i{727, 592, 562}, scanners[4].RotatedBeacons[0][0])
 	assert.Equal(t, Vector3i{30, -46, -14}, scanners[4].RotatedBeacons[0][25])
+}
+
+func Test_01_example(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	scanners, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	mainScanner := SearchAndConsume(scanners)
+
+	assert.Equal(t, 79, len(mainScanner.UniqueBeacons))
+}
+
+func Test_01(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	scanners, err := ParseInput(reader)
+	assert.Nil(t, err)
+
+	mainScanner := SearchAndConsume(scanners)
+
+	assert.Equal(t, -1, len(mainScanner.UniqueBeacons))
 }
