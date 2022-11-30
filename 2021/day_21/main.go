aa17127ae48a83d0f11339987af87a4f489ad1cb
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-12-01 00:33:31 +0100

    Day 21 / 02

--- 2021/day_21/main.go
+++ 2021/day_21/main.go
@@ -3,9 +3,21 @@ package day_21
 import "github.com/petr-ujezdsky/advent-of-code-go/utils"
 
 type Player struct {
-	Position int
-	Score    int
+	Position, Score int
 }
+
+type Couple struct {
+	CurrentPlayer, OtherPlayer Player
+}
+
+func NewCouple(currentPlayer, otherPlayer Player) Couple {
+	return Couple{
+		CurrentPlayer: currentPlayer,
+		OtherPlayer:   otherPlayer,
+	}
+}
+
+var dieRollOutcomes = dieRollOutcomesMap()
 
 func Play(p1, p2 Player, endScore int) (int, int, Player, Player) {
 	players := []*Player{&p1, &p2}
@@ -27,4 +39,76 @@ func Play(p1, p2 Player, endScore int) (int, int, Player, Player) {
 	dieRolls := turn * 3
 
 	return dieRolls * utils.Min(p1.Score, p2.Score), dieRolls, p1, p2
+}
+
+func dieRollOutcomesMapRecursive(outcomes map[int]int, height, sum int) {
+	if height == 0 {
+		outcomes[sum]++
+		return
+	}
+
+	// rolled 1
+	dieRollOutcomesMapRecursive(outcomes, height-1, sum+1)
+
+	// rolled 2
+	dieRollOutcomesMapRecursive(outcomes, height-1, sum+2)
+
+	// rolled 3
+	dieRollOutcomesMapRecursive(outcomes, height-1, sum+3)
+}
+
+// dieRollOutcomesMap creates map with
+// key - sum from 3 rolls
+// value - combinations count to get this sum
+func dieRollOutcomesMap() map[int]int {
+	// there will be 7 possible sums - from 3 (1+1+1) to 9 (3+3+3)
+	outcomes := make(map[int]int, 7)
+	dieRollOutcomesMapRecursive(outcomes, 3, 0)
+	return outcomes
+}
+
+func PlayRecursive(currentPlayer, otherPlayer Player, depth int, cache map[Couple][]int) (int, int) {
+	// check cache
+	couple := NewCouple(currentPlayer, otherPlayer)
+	counts, ok := cache[couple]
+	if ok {
+		// cache hit!
+		return counts[0], counts[1]
+	}
+
+	totalCurrentWins, totalOtherWins := 0, 0
+
+	for moves, count := range dieRollOutcomes {
+		position := (currentPlayer.Position+moves-1)%10 + 1
+		score := currentPlayer.Score + position
+
+		// current player is winner
+		if score >= 21 {
+			totalCurrentWins += count
+			continue
+		}
+
+		// update current player
+		updatedPlayer := Player{
+			Position: position,
+			Score:    score,
+		}
+
+		// next turn
+		otherWins, currentWins := PlayRecursive(otherPlayer, updatedPlayer, depth+1, cache)
+		totalCurrentWins += count * currentWins
+		totalOtherWins += count * otherWins
+	}
+
+	// cache results
+	cache[couple] = []int{totalCurrentWins, totalOtherWins}
+
+	return totalCurrentWins, totalOtherWins
+}
+
+func PlayFaster(p1, p2 Player) (int, int, int) {
+	cache := make(map[Couple][]int)
+	p1wins, p2wins := PlayRecursive(p1, p2, 0, cache)
+
+	return p1wins, p2wins, utils.Max(p1wins, p2wins)
 }
--- 2021/day_21/main_test.go
+++ 2021/day_21/main_test.go
@@ -5,6 +5,18 @@ import (
 	"testing"
 )
 
+func Test_dieRollOutcomes(t *testing.T) {
+	assert.Equal(t, 7, len(dieRollOutcomes))
+	assert.Equal(t, 1, dieRollOutcomes[3])
+	assert.Equal(t, 3, dieRollOutcomes[4])
+	assert.Equal(t, 6, dieRollOutcomes[5])
+	// there are 7 combinations to get sum of 6
+	assert.Equal(t, 7, dieRollOutcomes[6])
+	assert.Equal(t, 6, dieRollOutcomes[7])
+	assert.Equal(t, 3, dieRollOutcomes[8])
+	assert.Equal(t, 1, dieRollOutcomes[9])
+}
+
 func Test_01_example(t *testing.T) {
 	p1 := Player{
 		Position: 4,
@@ -41,4 +53,63 @@ func Test_01(t *testing.T) {
 	assert.Equal(t, 810, p2.Score)
 	assert.Equal(t, 747, dieRolls)
 	assert.Equal(t, 605070, result)
+}
+
+func Test_02_example(t *testing.T) {
+	p1 := Player{
+		Position: 4,
+		Score:    0,
+	}
+
+	p2 := Player{
+		Position: 8,
+		Score:    0,
+	}
+
+	p1wins, p2wins, max := PlayFaster(p1, p2)
+
+	assert.Equal(t, 444356092776315, p1wins)
+	assert.Equal(t, 341960390180808, p2wins)
+	assert.Equal(t, 444356092776315, max)
+}
+
+func Test_02(t *testing.T) {
+	p1 := Player{
+		Position: 8,
+		Score:    0,
+	}
+
+	p2 := Player{
+		Position: 10,
+		Score:    0,
+	}
+
+	p1wins, p2wins, max := PlayFaster(p1, p2)
+
+	assert.Equal(t, 218433063958910, p1wins)
+	assert.Equal(t, 189371397363999, p2wins)
+	assert.Equal(t, 218433063958910, max)
+}
+
+// Benchmark_fast-10    	     200	   5 966 090 ns/op
+func Benchmark_fast(b *testing.B) {
+	p1 := Player{
+		Position: 4,
+		Score:    0,
+	}
+
+	p2 := Player{
+		Position: 8,
+		Score:    0,
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		p1wins, p2wins, max := PlayFaster(p1, p2)
+
+		assert.Equal(b, 444356092776315, p1wins)
+		assert.Equal(b, 341960390180808, p2wins)
+		assert.Equal(b, 444356092776315, max)
+	}
 }
