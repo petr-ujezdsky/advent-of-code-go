6b41121fdec21bca145eea489a79dd88a8e511cf
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-12-05 01:14:26 +0100

    2021 day 22 / 02 cleanup

--- 2021/day_22/main.go
+++ 2021/day_22/main.go
@@ -5,288 +5,34 @@ import (
 	"fmt"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"io"
-	"math"
-	"regexp"
 	"strings"
 )
 
-type Vector3i = utils.Vector3i
-
-type IntersectionType int
-
-const (
-	None IntersectionType = iota
-	Partial
-	Inside
-	Wraps
-)
-
-var metric = utils.Metric{}
-
-type Cube struct {
-	Low, High Vector3i
-	Value     bool
-}
-
-func NewCubeSymmetric(halfSideLength int, value bool) Cube {
-	return Cube{
-		Low:   Vector3i{-halfSideLength, -halfSideLength, -halfSideLength},
-		High:  Vector3i{halfSideLength, halfSideLength, halfSideLength},
-		Value: value,
-	}
-}
-
-func NewCubeOrigin(sideLength int, value bool) Cube {
-	return Cube{
-		Low:   Vector3i{0, 0, 0},
-		High:  Vector3i{sideLength, sideLength, sideLength},
-		Value: value,
-	}
-}
-
-func (c Cube) Contains(p Vector3i) bool {
-	return c.Low.X <= p.X && p.X <= c.High.X &&
-		c.Low.Y <= p.Y && p.Y <= c.High.Y &&
-		c.Low.Z <= p.Z && p.Z <= c.High.Z
-}
-
-func (c Cube) ContainsWholeCube(c2 Cube) bool {
-	return c.Contains(c2.Low) && c.Contains(c2.High)
-}
-
-func intervalIntersection(lowA, highA, lowB, highB int) IntersectionType {
-	low, high, ok := utils.IntervalIntersection(lowA, highA, lowB, highB)
-
-	if !ok {
-		return None
-	}
-
-	// common interval is whole B -> whole B is inside A
-	if low == lowB && high == highB {
-		return Inside
-	}
-
-	// common interval is whole A -> B wraps the whole A
-	if low == lowA && high == highA {
-		return Wraps
-	}
-
-	// partial otherwise
-	return Partial
-}
-
-func (c Cube) Intersect(c2 Cube) IntersectionType {
-	ix := intervalIntersection(c.Low.X, c.High.X, c2.Low.X, c2.High.X)
-	iy := intervalIntersection(c.Low.Y, c.High.Y, c2.Low.Y, c2.High.Y)
-	iz := intervalIntersection(c.Low.Z, c.High.Z, c2.Low.Z, c2.High.Z)
-
-	// no intersection in at least 1 dimension -> no intersection at all
-	if ix == None || iy == None || iz == None {
-		return None
-	}
-
-	// c2 is inside in all dimensions -> c2 is inside c
-	if ix == Inside && iy == Inside && iz == Inside {
-		return Inside
-	}
-
-	// c2 wraps c in all dimensions -> c2 wraps c
-	if ix == Wraps && iy == Wraps && iz == Wraps {
-		return Wraps
-	}
-
-	// anything other is partial
-	return Partial
-}
-
-func (c Cube) Size() Vector3i {
-	return c.High.Subtract(c.Low).Add(Vector3i{1, 1, 1})
-}
-
-func (c Cube) Volume() int {
-	size := c.Size()
-	return size.X * size.Y * size.Z
-}
-
-//var splittingSteps = []Vector3i{
-//	{0, 0, 0},  // #1 no move (origin)
-//	{0, 1, 0},  // #2 backwards
-//	{1, 0, 0},  // #3 right
-//	{0, -1, 0}, // #4 onwards
-//	//{-1, 0, 1}, // #5 left & up
-//	//{0, 1, 0},  // #6 backwards
-//	//{1, 0, 0},  // #7 right
-//	//{0, -1, 0}, // #8 onwards
-//}
-
-func (c Cube) Split() []Cube {
-	size := c.High.Subtract(c.Low) //.Add(Vector3i{1, 1, 1})
-
-	if size == (Vector3i{0, 0, 0}) {
-		// can not divide single cell
-		panic("Can not divide single cell")
-	}
-
-	half := size.Divide(2)
-	halfAndOne := half.Add(Vector3i{1, 1, 1})
-
-	cubes := make([]Cube, 8)
-
-	i := 0
-	for kx := 0; kx < 2; kx++ {
-		for ky := 0; ky < 2; ky++ {
-			for kz := 0; kz < 2; kz++ {
-				k := Vector3i{kx, ky, kz}
-				low := c.Low.Add(halfAndOne.MultiplyParts(k))
-				high := low.Add(half)
-
-				cubes[i] = Cube{
-					Low:   low,
-					High:  high,
-					Value: c.Value,
-				}
-
-				i++
-			}
-		}
-	}
-
-	return cubes
-}
-
-var regexCube = regexp.MustCompile("(on|off) x=(-?\\d+)\\.\\.(-?\\d+),y=(-?\\d+)\\.\\.(-?\\d+),z=(-?\\d+)\\.\\.(-?\\d+)")
-
-func resolveOnOff(probe Cube, cubes []Cube) (bool, bool) {
-	for _, cube := range cubes {
-		intersectionType := cube.Intersect(probe)
-
-		switch intersectionType {
-		case None:
-		// continue with other cubes
-		case Inside:
-			// probe is inside the cube
-			return cube.Value, true
-		case Wraps, Partial:
-			return false, false
-		}
-	}
-
-	panic("Should match on whole world")
-	//return false, false
-}
-
-func countRecursive(probe Cube, cubes []Cube) int {
-	on, ok := resolveOnOff(probe, cubes)
-	if ok {
-		if on {
-			return probe.Volume()
-		}
-
-		return 0
-	}
-
-	// split
-	subProbes := probe.Split()
-	count := 0
-	for _, subProbe := range subProbes {
-		count += countRecursive(subProbe, cubes)
-	}
-
-	metric.TickSum(100_000, count)
-
-	return count
-}
-
-func FasterCount(world Cube, cubes []Cube) int {
-	// start investigation with *last* added cube and so on
-	cubes = utils.Reverse(cubes)
-
-	// make world size divisible by 2 and symmetric
-	size := world.Size()
-	maxSize := utils.Max(utils.Max(size.X, size.Y), size.Z)
-
-	nextPow2 := utils.NextPowOf2(maxSize + 1)
-	biggerWorld := NewCubeOrigin(nextPow2-1, false)
-
-	// shift world to original location
-	biggerWorld.Low = biggerWorld.Low.Add(world.Low)
-	biggerWorld.High = biggerWorld.High.Add(world.Low)
-
-	// final cube is world itself
-	cubes = append(cubes, biggerWorld)
-
-	return countRecursive(biggerWorld, cubes)
-}
-
-func NaiveCount(world Cube, cubes []Cube) int {
-	count := 0
-
-	// start investigation with *last* added cube and so on
-	cubes = utils.Reverse(cubes)
-
-	// final cube is world itself
-	cubes = append(cubes, world)
-
-	return count
-}
-
-func ParseInput(r io.Reader) ([]Cube, Cube) {
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanLines)
-
-	world := Cube{
-		Low:   utils.NewVector3nRepeated(math.MaxInt),
-		High:  utils.NewVector3nRepeated(math.MinInt),
-		Value: false,
-	}
-
-	var cubes []Cube
-	for scanner.Scan() {
-		parts := regexCube.FindStringSubmatch(scanner.Text())
-		x1, x2 := utils.ParseInt(parts[2]), utils.ParseInt(parts[3])
-		y1, y2 := utils.ParseInt(parts[4]), utils.ParseInt(parts[5])
-		z1, z2 := utils.ParseInt(parts[6]), utils.ParseInt(parts[7])
-
-		cube := Cube{
-			Low:   Vector3i{x1, y1, z1},
-			High:  Vector3i{x2, y2, z2},
-			Value: parts[1] == "on",
-		}
-
-		world.Low = world.Low.Min(cube.Low)
-		world.High = world.High.Max(cube.High)
-
-		cubes = append(cubes, cube)
-	}
-
-	return cubes, world
-}
-
 type IntervalI = utils.IntervalI
 
-type Cube2 struct {
+type Cube struct {
 	X, Y, Z IntervalI
 	On      bool
 }
 
-func (c Cube2) Volume() int {
+func (c Cube) Volume() int {
 	return c.X.Size() * c.Y.Size() * c.Z.Size()
 }
 
-func (c Cube2) Subtract(c2 Cube2) []Cube2 {
+func (c Cube) Subtract(c2 Cube) []Cube {
 	ix, ok := c.X.Intersection(c2.X)
 	if !ok {
-		return []Cube2{c}
+		return []Cube{c}
 	}
 
 	iy, ok := c.Y.Intersection(c2.Y)
 	if !ok {
-		return []Cube2{c}
+		return []Cube{c}
 	}
 
 	iz, ok := c.Z.Intersection(c2.Z)
 	if !ok {
-		return []Cube2{c}
+		return []Cube{c}
 	}
 
 	if c.X == ix && c.Y == iy && c.Z == iz {
@@ -300,7 +46,7 @@ func (c Cube2) Subtract(c2 Cube2) []Cube2 {
 	zs := []IntervalI{{c.Z.Low, iz.Low - 1}, iz, {iz.High + 1, c.Z.High}}
 
 	// generate all 27 sub-cubes
-	subs := make([]Cube2, 0, 27)
+	subs := make([]Cube, 0, 27)
 	for xj, xi := range xs {
 		if xi.IsInversed() {
 			// invalid interval - intersection is at the beginning (or end) of the cube
@@ -322,7 +68,7 @@ func (c Cube2) Subtract(c2 Cube2) []Cube2 {
 					continue
 				}
 
-				sub := Cube2{
+				sub := Cube{
 					X:  xi,
 					Y:  yi,
 					Z:  zi,
@@ -337,15 +83,15 @@ func (c Cube2) Subtract(c2 Cube2) []Cube2 {
 	return subs
 }
 
-func ExtraFastCount(cubes []Cube2) int {
-	var onCubes []Cube2
+func CountOnCubes(cubes []Cube) int {
+	var onCubes []Cube
 	for i, cube := range cubes {
 		if cube.On {
-			subs := []Cube2{cube}
+			subs := []Cube{cube}
 
 			// subtract all current on-cubes from cube
 			for _, onCube := range onCubes {
-				var newSubs []Cube2
+				var newSubs []Cube
 				for _, sub := range subs {
 					newSubs = append(newSubs, sub.Subtract(onCube)...)
 				}
@@ -353,7 +99,7 @@ func ExtraFastCount(cubes []Cube2) int {
 			}
 			onCubes = append(onCubes, subs...)
 		} else {
-			var newOnCubes []Cube2
+			var newOnCubes []Cube
 
 			// subtract cube from all current on-cubes
 			for _, onCube := range onCubes {
@@ -361,7 +107,7 @@ func ExtraFastCount(cubes []Cube2) int {
 			}
 			onCubes = newOnCubes
 		}
-		fmt.Printf("Cube #%v (%v), onCubes count: %v\n", i, cube.On, len(onCubes))
+		fmt.Printf("Cube #%v (%v), onCubes count: %v\n", i+1, cube.On, len(onCubes))
 	}
 
 	count := 0
@@ -372,18 +118,18 @@ func ExtraFastCount(cubes []Cube2) int {
 	return count
 }
 
-func ParseInput2(r io.Reader) []Cube2 {
+func ParseInput(r io.Reader) []Cube {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanLines)
 
-	var cubes []Cube2
+	var cubes []Cube
 	for scanner.Scan() {
 		line := scanner.Text()
 		ints := utils.ExtractInts(line, true)
 
 		on := strings.HasPrefix(line, "on")
 
-		cube := Cube2{
+		cube := Cube{
 			X:  utils.NewInterval(ints[0], ints[1]),
 			Y:  utils.NewInterval(ints[2], ints[3]),
 			Z:  utils.NewInterval(ints[4], ints[5]),
--- 2021/day_22/main_test.go
+++ 2021/day_22/main_test.go
@@ -7,108 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_regexCube(t *testing.T) {
-	matches := regexCube.FindStringSubmatch("on x=10..12,y=10..12,z=10..12")
-	assert.Equal(t, []string{"on x=10..12,y=10..12,z=10..12", "on", "10", "12", "10", "12", "10", "12"}, matches)
-
-	matches = regexCube.FindStringSubmatch("on x=-54112..-39298,y=-85059..-49293,z=-27449..7877")
-	assert.Equal(t, []string{"on x=-54112..-39298,y=-85059..-49293,z=-27449..7877", "on", "-54112", "-39298", "-85059", "-49293", "-27449", "7877"}, matches)
-}
-
-func Test_Split(t *testing.T) {
-	probe := Cube{
-		Low:   Vector3i{0, 0, 0},
-		High:  Vector3i{3, 3, 3},
-		Value: false,
-	}
-
-	singleCell := probe.Split()[0].Split()[0]
-	expected := Cube{
-		Low:   Vector3i{0, 0, 0},
-		High:  Vector3i{0, 0, 0},
-		Value: false,
-	}
-	assert.Equal(t, expected, singleCell)
-}
-
-func Test_Intersect(t *testing.T) {
-	probe := Cube{
-		Low:   Vector3i{0, 0, 0},
-		High:  Vector3i{0, 0, 0},
-		Value: false,
-	}
-
-	intersectionType := probe.Intersect(probe)
-	assert.Equal(t, Inside, intersectionType)
-}
-
-func Test_01_example_1_fast(t *testing.T) {
-	reader, err := os.Open("data-00-example-1.txt")
-	assert.Nil(t, err)
-
-	cubes := ParseInput2(reader)
-	assert.Equal(t, 4, len(cubes))
-
-	count := ExtraFastCount(cubes)
-	assert.Equal(t, 39, count)
-}
-
-func Test_01_example_2_fast(t *testing.T) {
-	reader, err := os.Open("data-00-example-2.txt")
-	assert.Nil(t, err)
-
-	cubes := ParseInput2(reader)
-	assert.Equal(t, 4, len(cubes))
-
-	count := ExtraFastCount(cubes[:20])
-	assert.Equal(t, 590784, count)
-}
-
-func Test_01_fast(t *testing.T) {
-	reader, err := os.Open("data-01.txt")
-	assert.Nil(t, err)
-
-	cubes := ParseInput2(reader)
-	assert.Equal(t, 420, len(cubes))
-
-	count := ExtraFastCount(cubes[:20])
-	assert.Equal(t, 620241, count)
-}
-
-func Test_02_example_3_fast(t *testing.T) {
-	reader, err := os.Open("data-00-example-3.txt")
-	assert.Nil(t, err)
-
-	cubes := ParseInput2(reader)
-	assert.Equal(t, 60, len(cubes))
-
-	metric.Enabled = true
-	count := ExtraFastCount(cubes)
-	assert.Equal(t, 2758514936282235, count)
-}
-
-func Test_02_fast(t *testing.T) {
-	reader, err := os.Open("data-01.txt")
-	assert.Nil(t, err)
-
-	cubes := ParseInput2(reader)
-	assert.Equal(t, 420, len(cubes))
-
-	count := ExtraFastCount(cubes)
-	assert.Equal(t, 1284561759639324, count)
-}
-
 func Test_01_example_1(t *testing.T) {
 	reader, err := os.Open("data-00-example-1.txt")
 	assert.Nil(t, err)
 
-	cubes, _ := ParseInput(reader)
+	cubes := ParseInput(reader)
 	assert.Equal(t, 4, len(cubes))
 
-	world := NewCubeSymmetric(50, false)
-
-	//count := NaiveCount(world, cubes)
-	count := FasterCount(world, cubes)
+	count := CountOnCubes(cubes)
 	assert.Equal(t, 39, count)
 }
 
@@ -116,13 +22,10 @@ func Test_01_example_2(t *testing.T) {
 	reader, err := os.Open("data-00-example-2.txt")
 	assert.Nil(t, err)
 
-	cubes, _ := ParseInput(reader)
+	cubes := ParseInput(reader)
 	assert.Equal(t, 22, len(cubes))
 
-	world := NewCubeSymmetric(50, false)
-
-	//count := NaiveCount(world, cubes[:20])
-	count := FasterCount(world, cubes[:20])
+	count := CountOnCubes(cubes[:20])
 	assert.Equal(t, 590784, count)
 }
 
@@ -130,13 +33,10 @@ func Test_01(t *testing.T) {
 	reader, err := os.Open("data-01.txt")
 	assert.Nil(t, err)
 
-	cubes, _ := ParseInput(reader)
+	cubes := ParseInput(reader)
 	assert.Equal(t, 420, len(cubes))
 
-	world := NewCubeSymmetric(50, false)
-
-	//count := NaiveCount(world, cubes[:20])
-	count := FasterCount(world, cubes[:20])
+	count := CountOnCubes(cubes[:20])
 	assert.Equal(t, 620241, count)
 }
 
@@ -144,45 +44,36 @@ func Test_02_example_3(t *testing.T) {
 	reader, err := os.Open("data-00-example-3.txt")
 	assert.Nil(t, err)
 
-	cubes, world := ParseInput(reader)
+	cubes := ParseInput(reader)
 	assert.Equal(t, 60, len(cubes))
 
-	metric.Enabled = true
-	//count := NaiveCount(world, cubes)
-	count := FasterCount(world, cubes)
+	count := CountOnCubes(cubes)
 	assert.Equal(t, 2758514936282235, count)
 }
 
-// Benchmark_fast-10    	      46	  25 154 525 ns/op
-func Benchmark_fast(b *testing.B) {
-	reader, err := os.Open("data-00-example-2.txt")
-	assert.Nil(b, err)
+func Test_02(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
 
-	cubes, _ := ParseInput(reader)
-	assert.Equal(b, 22, len(cubes))
+	cubes := ParseInput(reader)
+	assert.Equal(t, 420, len(cubes))
 
-	world := NewCubeSymmetric(50, false)
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		count := FasterCount(world, cubes[:20])
-		assert.Equal(b, 590784, count)
-	}
+	count := CountOnCubes(cubes)
+	assert.Equal(t, 1284561759639324, count)
 }
 
-// Benchmark_faster-10    	     556	   2 136 827 ns/op
-func Benchmark_faster(b *testing.B) {
+// Benchmark_CountOnCubes-10    	     556	   2 136 827 ns/op
+func Benchmark_CountOnCubes(b *testing.B) {
 	reader, err := os.Open("data-00-example-2.txt")
 	assert.Nil(b, err)
 
-	cubes := ParseInput2(reader)
+	cubes := ParseInput(reader)
 	assert.Equal(b, 22, len(cubes))
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		count := ExtraFastCount(cubes[:20])
+		count := CountOnCubes(cubes[:20])
 		assert.Equal(b, 590784, count)
 	}
 }
