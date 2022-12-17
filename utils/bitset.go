c8fcd2eee900396a81c360a17d003efdd4d5686f
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-12-18 00:50:31 +0100

    BitSet

---
+++ utils/bitset.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"fmt"
+	"math"
+)
+
+type UInteger interface {
+	uint8 | uint16 | uint32 | uint64
+}
+
+type BitSet[T UInteger] struct {
+	mask T
+}
+
+type BitSet8 = BitSet[uint8]
+type BitSet16 = BitSet[uint16]
+type BitSet32 = BitSet[uint32]
+type BitSet64 = BitSet[uint64]
+
+func NewEmptyBitSet[T UInteger]() BitSet[T] {
+	return NewBitSetInitialized[T](0)
+}
+
+func NewFullBitSet8() BitSet8 {
+	return NewBitSetInitialized[uint8](math.MaxUint8)
+}
+
+func NewFullBitSet16() BitSet16 {
+	return NewBitSetInitialized[uint16](math.MaxUint16)
+}
+
+func NewFullBitSet32() BitSet32 {
+	return NewBitSetInitialized[uint32](math.MaxUint32)
+}
+
+func NewFullBitSet64() BitSet64 {
+	return NewBitSetInitialized[uint64](math.MaxUint64)
+}
+
+func NewBitSetInitialized[T UInteger](mask T) BitSet[T] {
+	return BitSet[T]{mask}
+}
+
+func (s *BitSet[T]) Contains(value T) bool {
+	return (s.mask & (1 << value)) != 0
+}
+
+func (s *BitSet[T]) Push(value T) {
+	s.mask |= 1 << value
+}
+
+func (s *BitSet[T]) Remove(value T) {
+	s.mask &= ^(1 << value)
+}
+
+func (s *BitSet[T]) String() string {
+	return string(Reverse([]rune(fmt.Sprintf("%b", s.mask))))
+}
