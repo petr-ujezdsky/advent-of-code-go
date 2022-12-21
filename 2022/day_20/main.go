232d15b15920711ad42e2c335055a4fa7bf06b01
petr-ujezdsky <petr.ujezdsky@gmail.com>
2022-12-22 00:13:08 +0100

    2022 day 20 / 01 parsing

--- 2022/day_20/main.go
+++ 2022/day_20/main.go
@@ -6,10 +6,25 @@ import (
 	"io"
 )
 
-func MixNumbers(numbers []int) int {
+type PositionedNumber struct {
+	Value, Index int
+}
+
+func MixNumbers(numbers []PositionedNumber) int {
 	return len(numbers)
 }
 
-func ParseInput(r io.Reader) []int {
-	return utils.ParseToIntsP(r)
+func ParseInput(r io.Reader) []PositionedNumber {
+	ints := utils.ParseToIntsP(r)
+
+	var numbers []PositionedNumber
+
+	for i, v := range ints {
+		numbers = append(numbers, PositionedNumber{
+			Value: v,
+			Index: i,
+		})
+	}
+
+	return numbers
 }
