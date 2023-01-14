2c0fa10004e25280764bfeae302fc58d5f0b2332
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-01-15 00:10:36 +0100

    2020 day 16 / 01

---
+++ 2020/day_16/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	_ "embed"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils"
+	"io"
+	"strings"
+)
+
+type Rule struct {
+	Name           string
+	Range1, Range2 utils.IntervalI
+}
+
+func (r Rule) IsValid(v int) bool {
+	return r.Range1.Contains(v) || r.Range2.Contains(v)
+}
+
+type Ticket []int
+
+type World struct {
+	Rules        []Rule
+	MyTicket     Ticket
+	OtherTickets []Ticket
+}
+
+func DoWithInputPart01(world World) int {
+	invalidValuesSum := 0
+
+	for _, ticket := range world.OtherTickets {
+		for _, value := range ticket {
+			invalidCount := 0
+			for _, rule := range world.Rules {
+				if !rule.IsValid(value) {
+					invalidCount++
+				}
+			}
+
+			if invalidCount == len(world.Rules) {
+				invalidValuesSum += value
+			}
+		}
+	}
+
+	return invalidValuesSum
+}
+
+func DoWithInputPart02(world World) int {
+	return 0
+}
+
+func parseInterval(str string) utils.IntervalI {
+	numbers := utils.ExtractInts(str, false)
+	return utils.IntervalI{
+		Low:  numbers[0],
+		High: numbers[1],
+	}
+}
+
+func ParseInput(r io.Reader) World {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+
+	// rules
+	var rules []Rule
+	for scanner.Scan() && scanner.Text() != "" {
+		mainParts := strings.Split(scanner.Text(), ": ")
+		name := mainParts[0]
+
+		intervals := strings.Split(mainParts[1], " or ")
+
+		rules = append(rules, Rule{
+			Name:   name,
+			Range1: parseInterval(intervals[0]),
+			Range2: parseInterval(intervals[1]),
+		})
+	}
+
+	// my ticket
+	scanner.Scan()
+	scanner.Scan()
+	myTicket := Ticket(utils.ExtractInts(scanner.Text(), false))
+
+	// other tickets
+	scanner.Scan()
+	scanner.Scan()
+	var otherTickets []Ticket
+	for scanner.Scan() {
+		otherTickets = append(otherTickets, utils.ExtractInts(scanner.Text(), false))
+	}
+
+	return World{
+		Rules:        rules,
+		MyTicket:     myTicket,
+		OtherTickets: otherTickets,
+	}
+}
