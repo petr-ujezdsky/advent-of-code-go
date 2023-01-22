cd52d1b255837d9e25f03dccc1655951e422c4b1
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-01-23 00:45:25 +0100

    2020 day 21 / 01 working example

--- 2020/day_21/main.go
+++ 2020/day_21/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/maps"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/parsers"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/slices"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/strs"
@@ -19,8 +20,74 @@ type World struct {
 	Foods []Food
 }
 
+func findIntersection(s1, s2 StringSet) StringSet {
+	intersection := make(StringSet)
+	for v1 := range s1 {
+		if _, ok := s2[v1]; ok {
+			intersection[v1] = struct{}{}
+		}
+	}
+
+	return intersection
+}
+
+func removeIngredientAndAllergen(ingredient, allergen string, foods []Food) {
+	for _, food := range foods {
+		delete(food.Ingredients, ingredient)
+		delete(food.Allergens, allergen)
+	}
+}
+
+func findTranslation(foods []Food) (string, string, bool) {
+	for i, food1 := range foods {
+		// check direct translation
+		if len(food1.Ingredients) == 1 && len(food1.Allergens) == 1 {
+			ingredient := maps.FirstKey(food1.Ingredients)
+			allergen := maps.FirstKey(food1.Allergens)
+
+			return ingredient, allergen, true
+		}
+
+		// check intersections with other foods
+		for j := i + 1; j < len(foods); j++ {
+			food2 := foods[j]
+
+			sameAllergens := findIntersection(food1.Allergens, food2.Allergens)
+			if len(sameAllergens) == 1 {
+				sameIngredients := findIntersection(food1.Ingredients, food2.Ingredients)
+				if len(sameIngredients) == 1 {
+					ingredient := maps.FirstKey(sameIngredients)
+					allergen := maps.FirstKey(sameAllergens)
+
+					return ingredient, allergen, true
+				}
+			}
+		}
+	}
+
+	return "", "", false
+}
+
 func DoWithInputPart01(world World) int {
-	return 0
+	foundTranslations := make(map[string]string)
+	foods := world.Foods
+
+	for {
+		ingredient, allergen, ok := findTranslation(foods)
+		if !ok {
+			break
+		}
+
+		removeIngredientAndAllergen(ingredient, allergen, foods)
+		foundTranslations[ingredient] = allergen
+	}
+
+	count := 0
+	for _, food := range foods {
+		count += len(food.Ingredients)
+	}
+
+	return count
 }
 
 func DoWithInputPart02(world World) int {
--- 2020/day_21/main_test.go
+++ 2020/day_21/main_test.go
@@ -26,7 +26,7 @@ func Test_01_example(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 5, result)
 }
 
 func Test_01(t *testing.T) {
