aea079ea8a461504df7e152330125388a202afd7
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-10-27 02:29:19 +0200

    2019 day 03 / 01

---
+++ 2019/day_03/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	_ "embed"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils"
+	"io"
+	"math"
+	"strings"
+)
+
+type Wire struct {
+	Steps  []utils.Vector2i
+	Points []utils.Vector2i
+	Lines  []utils.LineOrthogonal2i
+}
+
+type World struct {
+	Wire1, Wire2 Wire
+}
+
+func DoWithInputPart01(world World) int {
+	return findNearestIntersection(world.Wire1.Lines, world.Wire2.Lines)
+}
+
+func findNearestIntersection(lines1, lines2 []utils.LineOrthogonal2i) int {
+	minDistance := math.MaxInt
+
+	for i, line1 := range lines1 {
+		for j, line2 := range lines2 {
+			// check intersection
+			if intersection, ok := line1.Intersection(line2); ok {
+				// intersection is point
+				if intersection.A == intersection.B {
+					// skip origin
+					if intersection.A == (utils.Vector2i{X: 0, Y: 0}) {
+						continue
+					}
+
+					minDistance = utils.Min(minDistance, intersection.A.LengthManhattan())
+				} else {
+					minDistance = i + j
+					panic("Intersection is not point - not implemented")
+				}
+			}
+		}
+	}
+
+	return minDistance
+}
+
+func DoWithInputPart02(world World) int {
+	return 0
+}
+
+func parseStep(step string) utils.Vector2i {
+	// first char is direction, rest is amount
+	dir := string(step[0])
+	amount := utils.ParseInt(step[1:])
+
+	var vec utils.Vector2i
+
+	switch dir {
+	case "R":
+		vec = utils.Vector2i{X: amount, Y: 0}
+	case "D":
+		vec = utils.Vector2i{X: 0, Y: -amount}
+	case "L":
+		vec = utils.Vector2i{X: -amount, Y: 0}
+	case "U":
+		vec = utils.Vector2i{X: 0, Y: amount}
+	default:
+		panic("Unknown direction " + dir)
+	}
+
+	return vec
+}
+
+func ParseInput(r io.Reader) World {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+
+	var wires []Wire
+	for scanner.Scan() {
+		stepsStr := strings.Split(scanner.Text(), ",")
+
+		steps := make([]utils.Vector2i, len(stepsStr))
+		points := make([]utils.Vector2i, len(stepsStr)+1)
+		lines := make([]utils.LineOrthogonal2i, len(stepsStr))
+
+		points[0] = utils.Vector2i{X: 0, Y: 0}
+
+		for i, stepStr := range stepsStr {
+			step := parseStep(stepStr)
+
+			steps[i] = step
+			points[i+1] = points[i].Add(step)
+			lines[i] = utils.NewLineOrthogonal2i(points[i], points[i+1])
+		}
+
+		wires = append(wires, Wire{
+			Steps:  steps,
+			Points: points,
+			Lines:  lines})
+	}
+
+	return World{
+		Wire1: wires[0],
+		Wire2: wires[1],
+	}
+}
---
+++ 2019/day_03/main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func Test_01_parse(t *testing.T) {
+	reader, err := os.Open("data-00-example-01.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	assert.Equal(t, 9, len(world.Wire1.Steps))
+	assert.Equal(t, 10, len(world.Wire1.Points))
+	assert.Equal(t, 8, len(world.Wire2.Steps))
+	assert.Equal(t, 9, len(world.Wire2.Points))
+}
+
+func Test_01_example_01(t *testing.T) {
+	reader, err := os.Open("data-00-example-01.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart01(world)
+	assert.Equal(t, 159, result)
+}
+
+func Test_01_example_02(t *testing.T) {
+	reader, err := os.Open("data-00-example-02.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart01(world)
+	assert.Equal(t, 135, result)
+}
+
+func Test_01_example_03(t *testing.T) {
+	reader, err := os.Open("data-00-example-03.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart01(world)
+	assert.Equal(t, 6, result)
+}
+
+func Test_01(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart01(world)
+	assert.Equal(t, 217, result)
+}
+
+func Test_02_example(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart02(world)
+	assert.Equal(t, 0, result)
+}
+
+func Test_02(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart02(world)
+	assert.Equal(t, 0, result)
+}
