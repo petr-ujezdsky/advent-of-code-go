f8166d32213dd4ac7686ba5fa187515f1015f508
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-12-01 14:37:32 +0000

    2023 updated template

--- 2023/_template/main.go
+++ 2023/_template/main.go
@@ -1,31 +1,57 @@
 package main
 
 import (
-	"bufio"
 	_ "embed"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/parsers"
 	"io"
 )
 
 type Item struct {
 }
 
-func DoWithInput(items []Item) int {
-	return len(items)
+type World struct {
+	Items []Item
+	//Matrix utils.Matrix[Item]
 }
 
-func ParseInput(r io.Reader) []Item {
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanLines)
+func DoWithInputPart01(world World) int {
+	return 0
+}
 
-	var items []Item
-	for scanner.Scan() {
-		//parts := strings.Split(scanner.Text(), ",")
-		//ints := utils.ExtractInts(scanner.Text(), false)
+func DoWithInputPart02(world World) int {
+	return 0
+}
 
-		item := Item{}
-
-		items = append(items, item)
+func ParseInput(r io.Reader) World {
+	parseItem := func(str string) Item {
+		return Item{}
 	}
 
-	return items
+	items := parsers.ParseToObjects(r, parseItem)
+	return World{Items: items}
 }
+
+//func ParseInput(r io.Reader) World {
+//	scanner := bufio.NewScanner(r)
+//	scanner.Split(bufio.ScanLines)
+//
+//	var items []Item
+//	for scanner.Scan() {
+//		//parts := strings.Split(scanner.Text(), ",")
+//		//ints := utils.ExtractInts(scanner.Text(), false)
+//
+//		item := Item{}
+//
+//		items = append(items, item)
+//	}
+//
+//	return World{Items: items}
+//}
+//
+//func ParseInput(r io.Reader) World {
+//	parseItem := func(char rune) Item {
+//		return Item{}
+//	}
+//
+//	return World{Matrix: parsers.ParseToMatrix(r, parseItem)}
+//}
