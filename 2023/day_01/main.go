a30cb3be2b2a596b771b3ad67da823beabe8ffab
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-12-02 00:39:34 +0000

    2023 day 01 / 02

--- 2023/day_01/main.go
+++ 2023/day_01/main.go
@@ -4,7 +4,9 @@ import (
 	_ "embed"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/parsers"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/strs"
 	"io"
+	"strings"
 )
 
 type World struct {
@@ -37,7 +39,83 @@ func extractNumber(row string) int {
 }
 
 func DoWithInputPart02(world World) int {
-	return 0
+	sum := 0
+
+	for _, row := range world.Rows {
+		sum += extractNumberWords(row)
+	}
+
+	return sum
+}
+
+var dictionaryForward = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+
+	"1": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
+}
+
+var dictionaryBackward = map[string]int{
+	"eno":   1,
+	"owt":   2,
+	"eerht": 3,
+	"ruof":  4,
+	"evif":  5,
+	"xis":   6,
+	"neves": 7,
+	"thgie": 8,
+	"enin":  9,
+
+	"1": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
+}
+
+func extractNumberWords(row string) int {
+	first := findFirstNumber(row, dictionaryForward)
+	last := findFirstNumber(strs.ReverseString(row), dictionaryBackward)
+
+	return first*10 + last
+}
+
+func findFirstNumber(str string, dic map[string]int) int {
+	index := 0
+	for {
+		if index >= len(str) {
+			break
+		}
+
+		for text, digit := range dic {
+			if strings.Index(str[index:], text) == 0 {
+				return digit
+			}
+		}
+
+		index++
+	}
+
+	panic("No number found")
 }
 
 func ParseInput(r io.Reader) World {
--- 2023/day_01/main_test.go
+++ 2023/day_01/main_test.go
@@ -56,14 +56,40 @@ func Test_01(t *testing.T) {
 	assert.Equal(t, 54940, result)
 }
 
+func Test_extractNumberWords(t *testing.T) {
+	type args struct {
+		row string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{"two1nine"}, 29},
+		{"", args{"eightwothree"}, 83},
+		{"", args{"abcone2threexyz"}, 13},
+		{"", args{"xtwone3four"}, 24},
+		{"", args{"4nineeightseven2"}, 42},
+		{"", args{"zoneight234"}, 14},
+		{"", args{"7pqrstsixteen"}, 76},
+
+		{"", args{"1zoneight"}, 18},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, extractNumberWords(tt.args.row), "extractNumberWords(%v)", tt.args.row)
+		})
+	}
+}
+
 func Test_02_example(t *testing.T) {
-	reader, err := os.Open("data-00-example.txt")
+	reader, err := os.Open("data-00-example-02.txt")
 	assert.Nil(t, err)
 
 	world := ParseInput(reader)
 
 	result := DoWithInputPart02(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 281, result)
 }
 
 func Test_02(t *testing.T) {
@@ -73,5 +99,5 @@ func Test_02(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart02(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 54208, result)
 }
