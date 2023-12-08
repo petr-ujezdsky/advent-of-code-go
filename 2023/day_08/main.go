13fc9bcf5bfc834c7ebfac8caed4220633cd8997
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-12-08 14:34:08 +0000

    2023 day 08 / 02 created Stepper

--- 2023/day_08/main.go
+++ 2023/day_08/main.go
@@ -18,10 +18,16 @@ const (
 
 var regexMapDef = regexp.MustCompile(`(...) = \((...), (...)\)`)
 
+type Shortcut struct {
+	EndMap *MapDef
+	Steps  int
+}
+
 type MapDef struct {
-	Name string
-	End  bool
-	Next [2]*MapDef
+	Name          string
+	End           bool
+	Next          [2]*MapDef
+	ShortcutToEnd map[int]Shortcut
 }
 
 type World struct {
@@ -31,46 +37,102 @@ type World struct {
 }
 
 func DoWithInputPart01(world World) int {
-	i := 0
-	current := world.Maps["AAA"]
+	stepper := &Stepper{
+		Directions: world.Directions,
+		Position:   0,
+		Current:    world.Maps["AAA"],
+		LastEnd:    nil,
+	}
 
-	for current.Name != "ZZZ" {
-		dir := world.Directions[utils.ModFloor(i, len(world.Directions))]
+	for stepper.Current.Name != "ZZZ" {
+		stepper.Move(1)
+	}
+
+	return stepper.Position
+}
+
+type Stepper struct {
+	Directions []Direction2
+	Position   int
+
+	Current *MapDef
+	LastEnd *MapDef
+}
+
+func (s *Stepper) Move(steps int) {
+	for i := 0; i < steps; i++ {
+		dirIndex := utils.ModFloor(s.Position, len(s.Directions))
+		dir := s.Directions[dirIndex]
+
+		next := s.Current.Next[dir]
+		s.Current = next
+		s.Position++
+	}
+}
+
+func DoWithInputPart02(world World) int {
+	i := 0
+
+	//histories := make([][]*MapDef, len(world.StartingMaps))
+	lastEnds := make([]*Shortcut, len(world.StartingMaps))
+	currents := slices.Clone(world.StartingMaps)
+	end := false
+
+	for !end {
+		dirIndex := utils.ModFloor(i, len(world.Directions))
+		dir := world.Directions[dirIndex]
+		end = true
+
+		for j := 0; j < len(currents); j++ {
+			// store current into history
+			//histories[j] = append(histories[j], currents[j])
+
+			next := currents[j].Next[dir]
+			currents[j] = next
+
+			if next.End {
+				// fill shortcuts
+				//for ih, mapHistory := range histories[j] {
+				//	//steps := i + 1 - ih
+				//	steps := len(histories[j]) - ih
+				//	Shortcut{
+				//		EndMap: next,
+				//		Steps:  0,
+				//	}
+				//	mapHistory.ShortcutToEnd[]
+				//}
+
+				// fill shortcut
+				lastEnd := lastEnds[j]
+				if lastEnd == nil {
+					lastEnds[j] = &Shortcut{
+						EndMap: next,
+						Steps:  i + 1,
+					}
+				}
+			} else {
+				// not ending map -> must continue
+				end = false
+			}
+		}
+
+		i++
+	}
+
+	return i
+}
+
+func countStepsToEnd(current *MapDef, directions []Direction2) int {
+	i := 0
+
+	for !current.End {
+		dir := directions[utils.ModFloor(i, len(directions))]
 		next := current.Next[dir]
 		current = next
 		i++
 	}
 
 	return i
-}
-
-func DoWithInputPart02(world World) int {
-	i := 0
-
-	currents := slices.Clone(world.StartingMaps)
-
-	for !isEnd(currents) {
-		dir := world.Directions[utils.ModFloor(i, len(world.Directions))]
-
-		for j := 0; j < len(currents); j++ {
-			next := currents[j].Next[dir]
-			currents[j] = next
-		}
-
-		i++
-	}
-
-	return i
-}
-
-func isEnd(maps []*MapDef) bool {
-	for _, mapDef := range maps {
-		if !mapDef.End {
-			return false
-		}
-	}
-
-	return true
 }
 
 func getOrCreateMapDef(name string, maps map[string]*MapDef) *MapDef {
