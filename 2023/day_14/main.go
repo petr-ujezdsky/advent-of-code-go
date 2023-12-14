6b10ff982f77dd290bdadc9f80db89792a4b6302
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-12-14 22:59:44 +0000

    2023 day 14 / 01

--- 2023/day_14/main.go
+++ 2023/day_14/main.go
@@ -20,12 +20,53 @@ type Tile struct {
 	Type TileType
 }
 
+func (t Tile) String() string {
+	return string(t.Char)
+}
+
 type World struct {
 	Tiles matrix.Matrix[Tile]
 }
 
 func DoWithInputPart01(world World) int {
-	return 0
+	totalLoad := MoveRocksUp(world.Tiles)
+
+	return totalLoad
+}
+
+func MoveRocksUp(tiles matrix.Matrix[Tile]) int {
+	totalLoad := 0
+
+	for x, column := range tiles.Columns {
+		emptyTileY := -1
+		for y, tile := range column {
+			char := tile.Char
+
+			switch char {
+			case '.':
+				if emptyTileY == -1 {
+					emptyTileY = y
+				}
+			case '#':
+				emptyTileY = -1
+			case 'O':
+				if emptyTileY != -1 {
+					// switch empty tile with rock
+					tiles.Columns[x][y], tiles.Columns[x][emptyTileY] = tiles.Columns[x][emptyTileY], tiles.Columns[x][y]
+
+					// update total load
+					totalLoad += len(column) - emptyTileY
+
+					// empty tile is right after the moved rock
+					emptyTileY++
+				} else {
+					totalLoad += len(column) - y
+				}
+			}
+		}
+	}
+
+	return totalLoad
 }
 
 func DoWithInputPart02(world World) int {
--- 2023/day_14/main_test.go
+++ 2023/day_14/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
@@ -16,6 +18,32 @@ func Test_01_parse(t *testing.T) {
 	assert.Equal(t, 10, world.Tiles.Height)
 }
 
+func TestMoveRocksUp(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	MoveRocksUp(world.Tiles)
+
+	str := world.Tiles.StringFmtSeparator("", func(tile Tile) string { return string(tile.Char) })
+	fmt.Println(str)
+
+	expected := utils.Msg(`
+OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#....`)
+
+	assert.Equal(t, expected, str)
+}
+
 func Test_01_example(t *testing.T) {
 	reader, err := os.Open("data-00-example.txt")
 	assert.Nil(t, err)
@@ -23,7 +51,7 @@ func Test_01_example(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 136, result)
 }
 
 func Test_01(t *testing.T) {
@@ -33,7 +61,7 @@ func Test_01(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 110128, result)
 }
 
 func Test_02_example(t *testing.T) {
