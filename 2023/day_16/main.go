cb0f3de7f447b2aa8d24e94a121c15c54239dae8
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-12-16 23:53:00 +0000

    2023 day 16 / 01

--- 2023/day_16/main.go
+++ 2023/day_16/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	_ "embed"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/matrix"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/parsers"
 	"io"
 )
 
 type Tile struct {
-	Char rune
+	Char    rune
+	NextFor [][]utils.Direction4
+	Visited []bool
 }
 
 type World struct {
@@ -16,7 +19,43 @@ type World struct {
 }
 
 func DoWithInputPart01(world World) int {
-	return 0
+	return Walk(world.Tiles)
+}
+
+func Walk(tiles matrix.Matrix[Tile]) int {
+	energized := make(map[utils.Vector2i]struct{})
+
+	walkRecursive(utils.Vector2i{X: -1, Y: tiles.Height - 1}, utils.Right, energized, tiles)
+
+	return len(energized)
+}
+
+func walkRecursive(position utils.Vector2i, direction utils.Direction4, energized map[utils.Vector2i]struct{}, tiles matrix.Matrix[Tile]) {
+	nextPosition := position.Add(direction.ToStep())
+	nextTile, ok := tiles.GetVSafe(nextPosition)
+
+	// outside the world
+	if !ok {
+		return
+	}
+
+	// already visited from this direction
+	if nextTile.Visited[direction] {
+		return
+	}
+
+	// mark as visited from this direction
+	nextTile.Visited[direction] = true
+
+	// energize
+	energized[nextPosition] = struct{}{}
+
+	// compute where to go next
+	nextDirections := nextTile.NextFor[direction]
+
+	for _, nextDirection := range nextDirections {
+		walkRecursive(nextPosition, nextDirection, energized, tiles)
+	}
 }
 
 func DoWithInputPart02(world World) int {
@@ -25,8 +64,54 @@ func DoWithInputPart02(world World) int {
 
 func ParseInput(r io.Reader) World {
 	parseItem := func(char rune) Tile {
-		return Tile{Char: char}
+		nextFor := make([][]utils.Direction4, 4)
+
+		switch char {
+		case '.':
+			{
+				nextFor[utils.Up] = []utils.Direction4{utils.Up}
+				nextFor[utils.Right] = []utils.Direction4{utils.Right}
+				nextFor[utils.Down] = []utils.Direction4{utils.Down}
+				nextFor[utils.Left] = []utils.Direction4{utils.Left}
+			}
+		case '\\':
+			{
+				nextFor[utils.Up] = []utils.Direction4{utils.Left}
+				nextFor[utils.Right] = []utils.Direction4{utils.Down}
+				nextFor[utils.Down] = []utils.Direction4{utils.Right}
+				nextFor[utils.Left] = []utils.Direction4{utils.Up}
+			}
+		case '/':
+			{
+				nextFor[utils.Up] = []utils.Direction4{utils.Right}
+				nextFor[utils.Right] = []utils.Direction4{utils.Up}
+				nextFor[utils.Down] = []utils.Direction4{utils.Left}
+				nextFor[utils.Left] = []utils.Direction4{utils.Down}
+			}
+		case '-':
+			{
+				nextFor[utils.Up] = []utils.Direction4{utils.Left, utils.Right}
+				nextFor[utils.Right] = []utils.Direction4{utils.Right}
+				nextFor[utils.Down] = []utils.Direction4{utils.Left, utils.Right}
+				nextFor[utils.Left] = []utils.Direction4{utils.Left}
+			}
+		case '|':
+			{
+				nextFor[utils.Up] = []utils.Direction4{utils.Up}
+				nextFor[utils.Right] = []utils.Direction4{utils.Up, utils.Down}
+				nextFor[utils.Down] = []utils.Direction4{utils.Down}
+				nextFor[utils.Left] = []utils.Direction4{utils.Up, utils.Down}
+			}
+		}
+
+		return Tile{
+			Char:    char,
+			NextFor: nextFor,
+			Visited: make([]bool, 4),
+		}
 	}
 
-	return World{Tiles: parsers.ParseToMatrix(r, parseItem)}
+	tiles := parsers.ParseToMatrix(r, parseItem).FlipVertical()
+
+	return World{Tiles: tiles}
 }
--- 2023/day_16/main_test.go
+++ 2023/day_16/main_test.go
@@ -24,7 +24,7 @@ func Test_01_example(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 46, result)
 }
 
 func Test_01(t *testing.T) {
@@ -34,7 +34,7 @@ func Test_01(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 6795, result)
 }
 
 func Test_02_example(t *testing.T) {
