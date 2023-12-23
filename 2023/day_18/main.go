9dee78fc501c6bd969df1c2a282be2a322006f2b
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-12-23 22:37:36 +0100

    2023 day 18 / 01 linked orders

--- 2023/day_18/main.go
+++ 2023/day_18/main.go
@@ -11,16 +11,14 @@ import (
 )
 
 type DigOrder struct {
-	Direction utils.Direction4
-	Amount    int
-	Color     string
-}
-
-type TrenchCube struct {
+	Direction      utils.Direction4
+	Amount         int
+	Color          string
+	Previous, Next *DigOrder
 }
 
 type World struct {
-	Orders  []DigOrder
+	Orders  []*DigOrder
 	Borders utils.BoundingRectangle
 }
 
@@ -30,15 +28,15 @@ func DoWithInputPart01(world World) int {
 	return LagoonArea(trench, bounds)
 }
 
-func WalkOrders(orders []DigOrder) (map[utils.Vector2i]DigOrder, utils.BoundingRectangle) {
-	trench := make(map[utils.Vector2i]DigOrder)
+func WalkOrders(orders []*DigOrder) (map[utils.Vector2i]*DigOrder, utils.BoundingRectangle) {
+	trench := make(map[utils.Vector2i]*DigOrder)
 
 	position := utils.Vector2i{}
 
 	bounds := utils.NewBoundingRectangleFromPoints(position, position)
 
 	// origin
-	trench[position] = DigOrder{
+	trench[position] = &DigOrder{
 		Direction: -1,
 		Amount:    -1,
 		Color:     "",
@@ -60,7 +58,7 @@ func WalkOrders(orders []DigOrder) (map[utils.Vector2i]DigOrder, utils.BoundingR
 	return trench, bounds
 }
 
-func LagoonArea(trench map[utils.Vector2i]DigOrder, bounds utils.BoundingRectangle) int {
+func LagoonArea(trench map[utils.Vector2i]*DigOrder, bounds utils.BoundingRectangle) int {
 	// use rendering algorithm
 	area := 0
 
@@ -70,12 +68,18 @@ func LagoonArea(trench map[utils.Vector2i]DigOrder, bounds utils.BoundingRectang
 	for x := bounds.Horizontal.Low; x <= bounds.Horizontal.High; x++ {
 		previous := '.'
 		crossingsCount := 0
+		areaY := 0
 		for y := bounds.Vertical.Low; y <= bounds.Vertical.High; y++ {
 			position := utils.Vector2i{X: x, Y: y}
 
 			current := '.'
-			if _, ok := trench[position]; ok {
+			if t, ok := trench[position]; ok {
 				current = '#'
+
+				// detect vertical
+				if t.Direction == utils.Up || t.Direction == utils.Down {
+
+				}
 			}
 
 			m.SetV(position.Subtract(mOrigin), current)
@@ -86,11 +90,14 @@ func LagoonArea(trench map[utils.Vector2i]DigOrder, bounds utils.BoundingRectang
 
 			if current == '#' || crossingsCount%2 == 1 {
 				// inside
-				area++
+				areaY++
 			}
 
 			previous = current
 		}
+		fmt.Printf("x=%d  area %d\n", x, areaY)
+
+		area += areaY
 	}
 
 	//m = m.FlipVertical()
@@ -123,14 +130,14 @@ func parseDir(s string) utils.Direction4 {
 	panic("Unknown direction")
 }
 func ParseInput(r io.Reader) World {
-	parseItem := func(str string) DigOrder {
+	parseItem := func(str string) *DigOrder {
 		parts := strings.Split(str, " ")
 
 		direction := parseDir(parts[0])
 		amount := utils.ParseInt(parts[1])
 		color := parts[2][1 : len(parts[2])-1]
 
-		return DigOrder{
+		return &DigOrder{
 			Direction: direction,
 			Amount:    amount,
 			Color:     color,
@@ -138,5 +145,15 @@ func ParseInput(r io.Reader) World {
 	}
 
 	orders := parsers.ParseToObjects(r, parseItem)
+
+	// link them
+	previous := orders[len(orders)-1]
+	for _, current := range orders {
+		current.Previous = previous
+		previous.Next = current
+
+		previous = current
+	}
+
 	return World{Orders: orders}
 }
