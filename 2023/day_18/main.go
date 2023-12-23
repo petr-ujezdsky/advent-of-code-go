ce7f492a9ed450877b2b5e7d9be38bfe8116bead
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-12-23 15:10:31 +0100

    2023 day 18 / 01 walk the trench and area rendering alg

--- 2023/day_18/main.go
+++ 2023/day_18/main.go
@@ -23,7 +23,70 @@ type World struct {
 }
 
 func DoWithInputPart01(world World) int {
-	return 0
+	trench, bounds := WalkOrders(world.Orders)
+
+	return LagoonArea(trench, bounds)
+}
+
+func WalkOrders(orders []DigOrder) (map[utils.Vector2i]DigOrder, utils.BoundingRectangle) {
+	trench := make(map[utils.Vector2i]DigOrder)
+
+	position := utils.Vector2i{}
+
+	bounds := utils.NewBoundingRectangleFromPoints(position, position)
+
+	// origin
+	trench[position] = DigOrder{
+		Direction: -1,
+		Amount:    -1,
+		Color:     "",
+	}
+
+	for _, order := range orders {
+		step := order.Direction.ToStep()
+
+		for i := 0; i < order.Amount; i++ {
+			nextPosition := position.Add(step)
+
+			trench[nextPosition] = order
+			bounds = bounds.Enlarge(nextPosition)
+
+			position = nextPosition
+		}
+	}
+
+	return trench, bounds
+}
+
+func LagoonArea(trench map[utils.Vector2i]DigOrder, bounds utils.BoundingRectangle) int {
+	// use rendering algorithm
+	area := 0
+
+	for x := bounds.Horizontal.Low; x <= bounds.Horizontal.High; x++ {
+		previous := '.'
+		crossingsCount := 0
+		for y := bounds.Vertical.Low; y <= bounds.Vertical.High; y++ {
+			position := utils.Vector2i{X: x, Y: y}
+
+			current := '.'
+			if _, ok := trench[position]; ok {
+				current = '#'
+			}
+
+			if previous != current {
+				crossingsCount++
+			}
+
+			if crossingsCount%2 == 1 {
+				// inside
+				area++
+			}
+
+			previous = current
+		}
+	}
+
+	return area
 }
 
 func DoWithInputPart02(world World) int {
