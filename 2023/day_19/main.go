e23abcb60ac9140e9d6dce2272d1740d15a05136
petr-ujezdsky <petr.ujezdsky@gmail.com>
2023-12-24 15:14:32 +0100

    2023 day 19 / 01 parse

---
+++ 2023/day_19/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	_ "embed"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/slices"
+	"io"
+	"regexp"
+	"strings"
+)
+
+type Category int
+
+const (
+	CategoryX Category = iota
+	CategoryM
+	CategoryA
+	CategoryS
+)
+
+type Part struct {
+	Ratings [4]int
+}
+
+type Workflow struct {
+	Name       string
+	Conditions []Condition
+	Fallback   *Workflow
+}
+
+type Condition struct {
+	Category Category
+	Operand  rune
+	Amount   int
+	Next     *Workflow
+}
+
+type World struct {
+	Workflows map[string]*Workflow
+	Start     *Workflow
+	Parts     []Part
+}
+
+func DoWithInputPart01(world World) int {
+	return 0
+}
+
+func DoWithInputPart02(world World) int {
+	return 0
+}
+
+func toCategory(str string) Category {
+	switch str {
+	case "x":
+		return CategoryX
+	case "m":
+		return CategoryM
+	case "a":
+		return CategoryA
+	case "s":
+		return CategoryS
+
+	}
+
+	panic("Unknown category " + str)
+}
+
+var partRegex = regexp.MustCompile(`\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}`)
+
+func ParsePart(str string) Part {
+	// {x=5,m=71,a=885,s=445}
+	ratings := partRegex.FindStringSubmatch(str)
+
+	return Part{Ratings: [4]int{
+		utils.ParseInt(ratings[1]),
+		utils.ParseInt(ratings[2]),
+		utils.ParseInt(ratings[3]),
+		utils.ParseInt(ratings[4]),
+	}}
+}
+
+var conditionRegex = regexp.MustCompile(`([xmas])([<>])(\d+):(.+)`)
+
+func ParseCondition(str string, workflows map[string]*Workflow) Condition {
+	parts := conditionRegex.FindStringSubmatch(str)
+
+	category := toCategory(parts[1])
+	operand := rune(parts[2][0])
+	amount := utils.ParseInt(parts[3])
+	next := getOrCreateWorkflow(parts[4], workflows)
+
+	return Condition{
+		Category: category,
+		Operand:  operand,
+		Amount:   amount,
+		Next:     next,
+	}
+}
+
+func ParseWorkFlow(str string, workflows map[string]*Workflow) {
+	mainParts := strings.Split(str, "{")
+
+	name := mainParts[0]
+	conditionsRaw := strings.Split(mainParts[1][:len(mainParts[1])-1], ",")
+	// proper conditions
+	conditions := slices.Map(conditionsRaw[:len(conditionsRaw)-1], func(s string) Condition {
+		return ParseCondition(s, workflows)
+	})
+	// fallback
+	fallback := workflows[conditionsRaw[len(conditionsRaw)-1]]
+
+	workflow := getOrCreateWorkflow(name, workflows)
+
+	workflow.Conditions = conditions
+	workflow.Fallback = fallback
+}
+
+func getOrCreateWorkflow(name string, workflows map[string]*Workflow) *Workflow {
+	if workflow, ok := workflows[name]; ok {
+		return workflow
+	}
+
+	workflow := &Workflow{
+		Name: name,
+	}
+
+	workflows[name] = workflow
+
+	return workflow
+}
+
+func ParseInput(r io.Reader) World {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+
+	// workflows
+	workflows := make(map[string]*Workflow)
+	for scanner.Scan() && len(scanner.Text()) > 0 {
+		ParseWorkFlow(scanner.Text(), workflows)
+	}
+
+	// parts
+	var parts []Part
+	for scanner.Scan() {
+		parts = append(parts, ParsePart(scanner.Text()))
+	}
+
+	return World{
+		Workflows: workflows,
+		Start:     workflows["in"],
+		Parts:     parts,
+	}
+}
---
+++ 2023/day_19/main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func Test_01_parse(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	assert.Equal(t, 11+2, len(world.Workflows))
+	assert.Equal(t, 5, len(world.Parts))
+
+	start := world.Start
+	assert.Equal(t, "in", start.Name)
+
+	condition := start.Conditions[0]
+	assert.Equal(t, CategoryS, condition.Category)
+	assert.Equal(t, '<', condition.Operand)
+	assert.Equal(t, 1351, condition.Amount)
+	assert.Equal(t, "px", condition.Next.Name)
+}
+
+func Test_01_example(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart01(world)
+	assert.Equal(t, 0, result)
+}
+
+func Test_01(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart01(world)
+	assert.Equal(t, 0, result)
+}
+
+func Test_02_example(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart02(world)
+	assert.Equal(t, 0, result)
+}
+
+func Test_02(t *testing.T) {
+	reader, err := os.Open("data-01.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	result := DoWithInputPart02(world)
+	assert.Equal(t, 0, result)
+}
