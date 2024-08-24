dab9ac7a5896f1f42e83600dea535f45f214e44b
Petr Újezdský <petr.ujezdsky@gmail.com>
2024-08-25 00:46:31 +0200

    2023 day 17 / 02

--- 2023/day_17/main.go
+++ 2023/day_17/main.go
@@ -90,28 +90,77 @@ func neighbours(m Matrix2i) func(origin State) []State {
 				continue
 			}
 
-			nextPos := origin.Position.Add(dir)
-
-			// check validity
-			if _, ok := m.GetVSafe(nextPos); !ok {
-				continue
+			if nextState, ok := dirStep(origin, dir, m, 1, 1, 3); ok {
+				neighbours = append(neighbours, nextState)
 			}
-
-			steps := 0
-			if origin.Direction == dir {
-				steps = origin.StepsInDirection + 1
-			}
-
-			if steps > 2 {
-				continue
-			}
-
-			nextState := State{Position: nextPos, Direction: dir, StepsInDirection: steps}
-			neighbours = append(neighbours, nextState)
 		}
 
 		return neighbours
 	}
+}
+
+func d2(m Matrix2i) func(State, State) int {
+	return func(nodeFrom, nodeTo State) int {
+		//dir := nodeTo.Position.Subtract(nodeFrom.Position).Signum()
+		revDir := nodeFrom.Position.Subtract(nodeTo.Position).Signum()
+
+		heatLoss := 0
+		for pos := nodeTo.Position; pos != nodeFrom.Position; pos = pos.Add(revDir) {
+			heatLoss += m.GetV(pos)
+		}
+
+		return heatLoss
+	}
+}
+
+func neighbours2(m Matrix2i) func(origin State) []State {
+	return func(origin State) []State {
+		var neighbours []State
+		for _, dir := range dirs {
+
+			// do not reverse
+			if dir == origin.Direction.Multiply(-1) {
+				continue
+			}
+
+			// step 1
+			if nextState, ok := dirStep(origin, dir, m, 1, 4, 10); ok {
+				neighbours = append(neighbours, nextState)
+			}
+
+			// step 4
+			if nextState, ok := dirStep(origin, dir, m, 4, 4, 10); ok {
+				neighbours = append(neighbours, nextState)
+			}
+		}
+
+		return neighbours
+	}
+}
+
+func dirStep(origin State, dir utils.Vector2i, m Matrix2i, stepSize, minStraight, maxStraight int) (State, bool) {
+	step := dir.Multiply(stepSize)
+	nextPos := origin.Position.Add(step)
+
+	// check validity
+	if _, ok := m.GetVSafe(nextPos); !ok {
+		return State{}, false
+	}
+
+	steps := stepSize
+	if origin.Direction == dir {
+		steps = origin.StepsInDirection + stepSize
+	}
+
+	if steps > maxStraight {
+		return State{}, false
+	}
+
+	if steps < minStraight {
+		return State{}, false
+	}
+
+	return State{Position: nextPos, Direction: dir, StepsInDirection: steps}, true
 }
 
 func isEnd(endPos utils.Vector2i) func(state State) bool {
@@ -125,8 +174,40 @@ func FindMinHeatLossPath(m Matrix2i) ([]State, map[State]int, int, bool) {
 	return alg.AStarEndFunc(State{}, isEnd(endPos), h(endPos), d(m), neighbours(m))
 }
 
+func FindMinHeatLossPath2(m Matrix2i) ([]State, map[State]int, int, bool) {
+	endPos := utils.Vector2i{X: m.Width - 1, Y: m.Height - 1}
+	return alg.AStarEndFunc(State{}, isEnd(endPos), h(endPos), d2(m), neighbours2(m))
+}
+
 func DoWithInputPart02(world World) int {
-	return 0
+	tiles := world.Tiles
+	path, _, totalHeatLoss, ok := FindMinHeatLossPath2(tiles)
+	if !ok {
+		panic("No path found")
+	}
+
+	pathMap := slices.ToMap(path, func(s State) utils.Vector2i { return s.Position })
+
+	dirStr := make(map[utils.Vector2i]string)
+	dirStr[dirs[0]] = "<"
+	dirStr[dirs[1]] = "^"
+	dirStr[dirs[2]] = ">"
+	dirStr[dirs[3]] = "v"
+
+	str := matrix.StringFmtSeparatorIndexed[int](tiles, "", func(value int, x, y int) string {
+		if s, ok := pathMap[utils.Vector2i{X: x, Y: y}]; ok {
+			if char, ok := dirStr[s.Direction]; ok {
+				return char
+			}
+			return "."
+		}
+
+		return strconv.Itoa(value)
+	})
+
+	fmt.Printf("Tiles:\n%v\n", str)
+
+	return totalHeatLoss
 }
 
 func ParseInput(r io.Reader) World {
