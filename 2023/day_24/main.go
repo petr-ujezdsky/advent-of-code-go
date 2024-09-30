f0a2ebce1d1ac51e4e10994448f39b8884c44cc3
Petr Újezdský <petr.ujezdsky@gmail.com>
2024-09-30 18:43:21 +0200

    2023 day 24 / 01

--- 2023/day_24/main.go
+++ 2023/day_24/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	_ "embed"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/equations"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/matrix"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/parsers"
 	"io"
 	"strings"
@@ -19,8 +21,68 @@ type World struct {
 	Lines []Line
 }
 
-func DoWithInputPart01(world World) int {
-	return 0
+func findCrossing2D(line1, line2 Line) (utils.Vector2f, bool) {
+	A := matrix.NewMatrixNumberRowNotation([][]float64{
+		{float64(line1.Direction.X), float64(-line2.Direction.X)},
+		{float64(line1.Direction.Y), float64(-line2.Direction.Y)},
+	})
+
+	b := utils.VectorNf{Items: []float64{
+		float64(line2.Position.X - line1.Position.X),
+		float64(line2.Position.Y - line1.Position.Y),
+	}}
+
+	ts, ok := equations.SolveLinearEquations(A, b)
+	if !ok {
+		return utils.Vector2f{}, false
+	}
+
+	if ts.Items[0] < 0 || ts.Items[1] < 0 {
+		// intersection in past -> do not use
+		return utils.Vector2f{}, false
+	}
+
+	x := utils.Vector2f{
+		X: float64(line1.Position.X) + ts.Items[0]*float64(line1.Direction.X),
+		Y: float64(line1.Position.Y) + ts.Items[0]*float64(line1.Direction.Y),
+	}
+
+	return x, true
+}
+
+func withinBounds(v utils.Vector2f, low, high float64) bool {
+	if v.X > high || v.Y > high {
+		return false
+	}
+
+	if v.X < low || v.Y < low {
+		return false
+	}
+
+	return true
+}
+
+func DoWithInputPart01(world World, low, high float64) int {
+	lines := world.Lines
+	count := 0
+
+	for i := 0; i < len(lines)-1; i++ {
+		for j := i + 1; j < len(lines); j++ {
+			line1 := lines[i]
+			line2 := lines[j]
+
+			cross, ok := findCrossing2D(line1, line2)
+			if !ok {
+				continue
+			}
+
+			if withinBounds(cross, low, high) {
+				count++
+			}
+		}
+	}
+
+	return count
 }
 
 func DoWithInputPart02(world World) int {
--- 2023/day_24/main_test.go
+++ 2023/day_24/main_test.go
@@ -18,14 +18,29 @@ func Test_01_parse(t *testing.T) {
 	assert.Equal(t, utils.Vector3i{X: 1, Y: -5, Z: -3}, world.Lines[4].Direction)
 }
 
+func Test_findCrossing2D(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+
+	line1 := world.Lines[1]
+	line2 := world.Lines[3]
+
+	cross, ok := findCrossing2D(line1, line2)
+
+	assert.True(t, ok)
+	assert.Equal(t, utils.Vector2f{X: -6, Y: -5}, cross)
+}
+
 func Test_01_example(t *testing.T) {
 	reader, err := os.Open("data-00-example.txt")
 	assert.Nil(t, err)
 
 	world := ParseInput(reader)
 
-	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	result := DoWithInputPart01(world, 7, 27)
+	assert.Equal(t, 2, result)
 }
 
 func Test_01(t *testing.T) {
@@ -34,8 +49,8 @@ func Test_01(t *testing.T) {
 
 	world := ParseInput(reader)
 
-	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	result := DoWithInputPart01(world, 200000000000000, 400000000000000)
+	assert.Equal(t, 18651, result)
 }
 
 func Test_02_example(t *testing.T) {
