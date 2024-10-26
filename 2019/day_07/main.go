480576aabb9740c6c89a20d600c8b189bd674ee9
Petr Újezdský <petr.ujezdsky@gmail.com>
2024-10-27 01:22:11 +0200

    2019 day 07 / 02

--- 2019/day_07/main.go
+++ 2019/day_07/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/petr-ujezdsky/advent-of-code-go/2019/common"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/combi"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/slices"
 	"io"
 )
 
@@ -15,18 +16,45 @@ type World struct {
 }
 
 func runSetting(phases [5]int, program []int) int {
-	input := 0
+	chEA := make(chan int, 1)
+	chAB := make(chan int, 1)
+	chBC := make(chan int, 1)
+	chCD := make(chan int, 1)
+	chDE := make(chan int, 1)
+	end := make(chan int)
 
-	for _, phase := range phases {
-		input = common.RunProgram([]int{phase, input}, program)
+	// init computers
+	computers := [5]common.IntCodeComputer{
+		common.NewIntCodeComputer("A", slices.Clone(program), chEA, chAB, nil),
+		common.NewIntCodeComputer("B", slices.Clone(program), chAB, chBC, nil),
+		common.NewIntCodeComputer("C", slices.Clone(program), chBC, chCD, nil),
+		common.NewIntCodeComputer("D", slices.Clone(program), chCD, chDE, nil),
+		common.NewIntCodeComputer("E", slices.Clone(program), chDE, chEA, end),
 	}
 
-	return input
+	// start computers
+	for _, computer := range computers {
+		go common.Run(computer)
+	}
+
+	// first input is phase
+	go func() {
+		chEA <- phases[0]
+		chAB <- phases[1]
+		chBC <- phases[2]
+		chCD <- phases[3]
+		chDE <- phases[4]
+
+		// start off with 0
+		chEA <- 0
+	}()
+
+	// wait for E to halt
+	return <-end
 }
 
-func DoWithInputPart01(world World) int {
+func findMaxWithPhaseValues(initialPhases []int, world World) int {
 	quit := make(chan interface{})
-	initialPhases := []int{0, 1, 2, 3, 4}
 	phases := combi.Permute(quit, initialPhases)
 
 	maximum := -1
@@ -48,8 +76,12 @@ func DoWithInputPart01(world World) int {
 	return maximum
 }
 
+func DoWithInputPart01(world World) int {
+	return findMaxWithPhaseValues([]int{0, 1, 2, 3, 4}, world)
+}
+
 func DoWithInputPart02(world World) int {
-	return 0
+	return findMaxWithPhaseValues([]int{5, 6, 7, 8, 9}, world)
 }
 
 func ParseInput(r io.Reader) World {
--- 2019/day_07/main_test.go
+++ 2019/day_07/main_test.go
@@ -52,14 +52,22 @@ func Test_01(t *testing.T) {
 	assert.Equal(t, 14902, result)
 }
 
-func Test_02_example(t *testing.T) {
-	reader, err := os.Open("data-00-example.txt")
-	assert.Nil(t, err)
-
-	world := ParseInput(reader)
+func Test_02_example_1(t *testing.T) {
+	world := World{
+		Program: []int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+	}
 
 	result := DoWithInputPart02(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 139629729, result)
+}
+
+func Test_02_example_2(t *testing.T) {
+	world := World{
+		Program: []int{3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10},
+	}
+
+	result := DoWithInputPart02(world)
+	assert.Equal(t, 18216, result)
 }
 
 func Test_02(t *testing.T) {
@@ -69,5 +77,5 @@ func Test_02(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart02(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 6489132, result)
 }
