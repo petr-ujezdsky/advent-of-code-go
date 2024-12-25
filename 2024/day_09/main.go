352abe1b2ff4f445a9621f63f00770361c52035b
Petr Újezdský <petr.ujezdsky@gmail.com>
2024-12-26 00:42:56 +0100

    2024 day 09 / 01

--- 2024/day_09/main.go
+++ 2024/day_09/main.go
@@ -3,15 +3,162 @@ package main
 import (
 	"bufio"
 	_ "embed"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"io"
+	"strconv"
+	"strings"
 )
 
 type World struct {
 	DiskMap []int
 }
 
+type Block struct {
+	FileId         int
+	Length         int
+	Previous, Next *Block
+}
+
+func (block *Block) Split(leftLength int) (*Block, *Block) {
+	b1 := block
+	b2 := &Block{
+		FileId:   block.FileId,
+		Length:   b1.Length - leftLength,
+		Previous: b1,
+		Next:     block.Next,
+	}
+
+	b1.Next = b2
+	b1.Length = leftLength
+
+	return b1, b2
+}
+
+type Blocks struct {
+	Head, Tail *Block
+}
+
+func (blocks Blocks) String() string {
+	var sb strings.Builder
+
+	for block := blocks.Head; block != nil; block = block.Next {
+		for i := 0; i < block.Length; i++ {
+			if block.FileId == -1 {
+				sb.WriteRune('.')
+			} else {
+				sb.WriteString(strconv.Itoa(block.FileId))
+			}
+		}
+
+	}
+
+	return sb.String()
+}
+
+func (blocks Blocks) Checksum() int {
+	sum := 0
+	position := 0
+
+	for block := blocks.Head; block != nil; block = block.Next {
+		if block.FileId != -1 {
+			sum += block.FileId * utils.SumNtoM(position, position+block.Length-1)
+		}
+
+		position += block.Length
+	}
+
+	return sum
+}
+
+func parseBlocks(diskMap []int) Blocks {
+	fileIdSeq := 0
+
+	var head *Block
+	var previous *Block
+	for i, size := range diskMap {
+		isFile := i%2 == 0
+
+		fileId := -1
+		if isFile {
+			fileId = fileIdSeq
+			fileIdSeq++
+		}
+
+		block := &Block{
+			FileId:   fileId,
+			Length:   size,
+			Previous: previous,
+			Next:     nil,
+		}
+
+		if head == nil {
+			head = block
+		}
+
+		if previous != nil {
+			previous.Next = block
+		}
+
+		previous = block
+	}
+
+	return Blocks{
+		Head: head,
+		Tail: previous,
+	}
+}
+
+func printBlocks(blocks Blocks) {
+	//fmt.Println(blocks.String())
+}
+
 func DoWithInputPart01(world World) int {
-	return 0
+	blocks := parseBlocks(world.DiskMap)
+	printBlocks(blocks)
+
+	lastFile := blocks.Tail
+	i := 0
+	for block := blocks.Head; block != nil; block = block.Next {
+		i++
+		if i > 100000 {
+			panic("Too much")
+		}
+		if block == lastFile {
+			break
+		}
+		if block.FileId != -1 {
+			// skip files
+			continue
+		}
+
+		// find last file block
+		for lastFile.FileId == -1 {
+			lastFile = lastFile.Previous
+		}
+
+		usableSize := utils.Min(block.Length, lastFile.Length)
+
+		if block.Length > usableSize {
+			block.Split(usableSize)
+		}
+
+		block.FileId = lastFile.FileId
+		lastFile.Length -= usableSize
+
+		if lastFile.Length == 0 {
+			// remove it
+			lastFile.Previous.Next = lastFile.Next
+			if lastFile.Next != nil {
+				lastFile.Next.Previous = lastFile.Previous
+			}
+
+			lastFile = lastFile.Previous
+		}
+
+		printBlocks(blocks)
+	}
+
+	return blocks.Checksum()
 }
 
 func DoWithInputPart02(world World) int {
--- 2024/day_09/main_test.go
+++ 2024/day_09/main_test.go
@@ -15,6 +15,22 @@ func Test_01_parse(t *testing.T) {
 	assert.Equal(t, 19, len(world.DiskMap))
 }
 
+func Test_01_parseBlocks(t *testing.T) {
+	blocks := parseBlocks([]int{1, 2, 3, 4, 5})
+
+	assert.Equal(t, "0..111....22222", blocks.String())
+}
+
+func Test_01_parseBlocks2(t *testing.T) {
+	reader, err := os.Open("data-00-example.txt")
+	assert.Nil(t, err)
+
+	world := ParseInput(reader)
+	blocks := parseBlocks(world.DiskMap)
+
+	assert.Equal(t, "00...111...2...333.44.5555.6666.777.888899", blocks.String())
+}
+
 func Test_01_example(t *testing.T) {
 	reader, err := os.Open("data-00-example.txt")
 	assert.Nil(t, err)
@@ -22,7 +38,7 @@ func Test_01_example(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 1928, result)
 }
 
 func Test_01(t *testing.T) {
@@ -32,7 +48,7 @@ func Test_01(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 6344673854800, result)
 }
 
 func Test_02_example(t *testing.T) {
