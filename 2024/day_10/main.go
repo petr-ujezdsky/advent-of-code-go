1645d855a3cf1af5495b36770629653b11c5811e
Petr Újezdský <petr.ujezdsky@gmail.com>
2024-12-27 23:44:09 +0100

    2024 day 10 / 01

--- 2024/day_10/main.go
+++ 2024/day_10/main.go
@@ -2,19 +2,58 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/matrix"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/parsers"
 	"io"
 )
 
+type VecSet map[utils.Vector2i]struct{}
+
 type World struct {
 	Matrix     matrix.Matrix[int]
 	TrailHeads []utils.Vector2i
 }
 
+func walk(pos, from utils.Vector2i, height int, m matrix.Matrix[int], peaks VecSet) {
+	if height == 9 {
+		peaks[pos] = struct{}{}
+		return
+	}
+
+	for _, step := range utils.Direction4Steps {
+		nextPos := pos.Add(step)
+		if nextPos == from {
+			continue
+		}
+
+		nextHeight, ok := m.GetVSafe(nextPos)
+		if !ok {
+			continue
+		}
+
+		if nextHeight != height+1 {
+			continue
+		}
+
+		walk(nextPos, pos, nextHeight, m, peaks)
+	}
+}
+
 func DoWithInputPart01(world World) int {
-	return 0
+	score := 0
+
+	for i, trailHead := range world.TrailHeads {
+		fmt.Printf("Trying trailhead #%v: ", i)
+		peaks := make(VecSet)
+		walk(trailHead, utils.Vector2i{X: -1, Y: -1}, 0, world.Matrix, peaks)
+		count := len(peaks)
+		fmt.Printf("%vx\n", count)
+		score += count
+	}
+
+	return score
 }
 
 func DoWithInputPart02(world World) int {
--- 2024/day_10/main_test.go
+++ 2024/day_10/main_test.go
@@ -24,7 +24,7 @@ func Test_01_example(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 36, result)
 }
 
 func Test_01(t *testing.T) {
@@ -34,7 +34,7 @@ func Test_01(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 717, result)
 }
 
 func Test_02_example(t *testing.T) {
