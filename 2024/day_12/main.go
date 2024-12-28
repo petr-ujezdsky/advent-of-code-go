3e94d1e78b2408d0f74b86a854e457a98ccb4138
Petr Újezdský <petr.ujezdsky@gmail.com>
2024-12-28 23:18:46 +0100

    2024 day 12 / 01 parse

---
+++ 2024/day_12/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	_ "embed"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/matrix"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/parsers"
+	"io"
+)
+
+type World struct {
+	Matrix matrix.Matrix[rune]
+}
+
+func DoWithInputPart01(world World) int {
+	return 0
+}
+
+func DoWithInputPart02(world World) int {
+	return 0
+}
+
+func ParseInput(r io.Reader) World {
+	parseItem := func(char rune) rune {
+		return char
+	}
+
+	return World{Matrix: parsers.ParseToMatrix(r, parseItem)}
+}
