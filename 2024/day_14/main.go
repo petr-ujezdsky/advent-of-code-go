86bf06b386c25cb86591f06ae57115c6083b7cbd
Petr Újezdský <petr.ujezdsky@gmail.com>
2024-12-31 00:54:51 +0100

    2024 day 14 / 02 finding Christmas tree

--- 2024/day_14/main.go
+++ 2024/day_14/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/matrix"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/parsers"
 	"io"
+	"strconv"
 )
 
 type Robot struct {
@@ -29,14 +32,7 @@ func DoWithInputPart01(world World, width int, height int) int {
 
 	quadrants := createQuadrants(width, height)
 
-	counts := [4]int{}
-	for _, robot := range world.Robots {
-		for i, quadrant := range quadrants {
-			if quadrant.Contains(robot.Position) {
-				counts[i]++
-			}
-		}
-	}
+	counts := countsInBounds(quadrants, world.Robots)
 	return counts[0] * counts[1] * counts[2] * counts[3]
 }
 
@@ -65,7 +61,78 @@ func createQuadrants(width, height int) []utils.BoundingRectangle {
 	return quadrants
 }
 
+func printRobots(robots []*Robot, bounds []utils.BoundingRectangle, width int, height int) {
+	m := matrix.NewMatrix[int](width, height)
+
+	for _, robot := range robots {
+		pos := robot.Position
+
+		m.SetV(pos, m.GetV(pos)+1)
+	}
+
+	formatter := func(i, x, y int) string {
+		if i > 9 {
+			panic("Too much")
+		}
+
+		for _, bound := range bounds {
+			if bound.Contains(utils.Vector2i{X: x, Y: y}) {
+				return "x"
+			}
+		}
+
+		if i == 0 {
+			return " "
+		}
+		return strconv.Itoa(i)
+	}
+
+	fmt.Println(matrix.StringFmtSeparatorIndexed(m, false, "", formatter))
+}
+
+func countsInBounds(bounds []utils.BoundingRectangle, robots []*Robot) []int {
+	counts := make([]int, len(bounds))
+
+	for _, robot := range robots {
+		for i, bound := range bounds {
+			if bound.Contains(robot.Position) {
+				counts[i]++
+			}
+		}
+	}
+
+	return counts
+}
+
 func DoWithInputPart02(world World, width int, height int) int {
+	qw := 21
+	qh := 7
+	corners := []utils.BoundingRectangle{
+		{
+			Horizontal: utils.IntervalI{High: qw - 1},
+			Vertical:   utils.IntervalI{High: qh - 1},
+		},
+		{
+			Horizontal: utils.IntervalI{Low: width - qw, High: width - 1},
+			Vertical:   utils.IntervalI{High: qh - 1},
+		},
+	}
+
+	seconds := 0
+	for count := 0; count < 3*10; {
+		counts := countsInBounds(corners, world.Robots)
+		if counts[0]+counts[1] == 0 {
+			count++
+			fmt.Printf("After %3d seconds -----------------------------------------------------------------------------------\n", seconds)
+			printRobots(world.Robots, corners, width, height)
+		}
+		play(1, width, height, world.Robots)
+		seconds++
+		if seconds%1_000_000 == 0 {
+			fmt.Printf("%v. second...\n", seconds)
+		}
+	}
+
 	return 0
 }
 
