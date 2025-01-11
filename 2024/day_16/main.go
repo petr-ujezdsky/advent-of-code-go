ed93ff61bfb8da486e296301bece5f6ce7cf58e7
Petr Újezdský <petr.ujezdsky@gmail.com>
2025-01-12 00:27:02 +0100

    2024 day 16 / 01

--- 2024/day_16/main.go
+++ 2024/day_16/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/alg"
+	"github.com/petr-ujezdsky/advent-of-code-go/utils/iterators"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/matrix"
 	"github.com/petr-ujezdsky/advent-of-code-go/utils/parsers"
 	"io"
@@ -13,9 +16,77 @@ type World struct {
 	Start, End utils.Vector2i
 }
 
-func DoWithInputPart01(world World) int {
+type State struct {
+	Position  utils.Vector2i
+	Direction utils.Vector2i
+}
 
-	return 0
+func h(endPos utils.Vector2i) func(state State) int {
+	return func(state State) int {
+		return utils.ManhattanDistance(state.Position, endPos)
+	}
+}
+
+func d() func(State, State) int {
+	return func(nodeFrom, nodeTo State) int {
+		if nodeFrom.Direction == nodeTo.Direction {
+			return 1
+		}
+
+		return 1001
+	}
+}
+
+func n(m matrix.Matrix[string]) func(origin State, path iterators.Iterator[State]) []State {
+	return func(origin State, path iterators.Iterator[State]) []State {
+		var neighbours []State
+		for _, dir := range utils.Direction4Steps {
+
+			// do not reverse
+			if dir == origin.Direction.Multiply(-1) {
+				continue
+			}
+
+			nextPos := origin.Position.Add(dir)
+
+			if nn, ok := m.GetVSafe(nextPos); !ok || nn == "#" {
+				continue
+			}
+
+			nextState := State{
+				Position:  nextPos,
+				Direction: dir,
+			}
+
+			neighbours = append(neighbours, nextState)
+		}
+
+		return neighbours
+	}
+}
+
+func isEnd(endPos utils.Vector2i) func(state State) bool {
+	return func(state State) bool {
+		return state.Position == endPos
+	}
+}
+
+func DoWithInputPart01(world World) int {
+	start := State{
+		Position:  world.Start,
+		Direction: utils.Right.ToStep(),
+	}
+
+	path, _, score, _ := alg.AStarEndFunc(start, isEnd(world.End), h(world.End), d(), n(world.Matrix))
+
+	for _, state := range path {
+		world.Matrix.SetV(state.Position, "x")
+	}
+	formatter := matrix.NonIndexedAdapter(matrix.FmtFmt[string]("%s"))
+	str := matrix.StringFmtSeparatorIndexed(world.Matrix, true, "", formatter)
+	fmt.Println(str)
+
+	return score
 }
 
 func DoWithInputPart02(world World) int {
--- 2024/day_16/main_test.go
+++ 2024/day_16/main_test.go
@@ -34,7 +34,7 @@ func Test_01(t *testing.T) {
 	world := ParseInput(reader)
 
 	result := DoWithInputPart01(world)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 92432, result)
 }
 
 func Test_02_example(t *testing.T) {
